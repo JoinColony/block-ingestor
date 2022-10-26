68507faa06ae7de2165a317c2fb91f01b98b9e9d
Alicja Kujawa <alicjak94.ak@gmail.com>
2022-10-26 12:31:49 +0200

    Subscribe to existing whitelists, handle user approved events

--- handlers/whitelist.js
+++ handlers/whitelist.js
@@ -1,12 +1,14 @@
-function handleUserApproved(args, whitelistAddress) {
-  const { user, status } = args;
-  // TODO find out where to store this informations
+const { poorMansGraphQL } = require('../utils');
+
+function handleUserStatusCreate(user, whitelistAddress, status = null, agreement = null) {
+  const input = status ? `{ walletAddress: "${user}", approved: ${Boolean(status)}, whitelistID: "${whitelistAddress}" }` :
+  `{ walletAddress: "${user}", agreementSigned: true, whitelistID: "${whitelistAddress}" }`
   const query = {
-    operationName: "UpdateWhitelist",
+    operationName: "CreateUserStatusMutation",
     query: `
-        mutation UpdateWhitelist {
-          updateWhitelist(
-          input: { id: "${whitelistAddress}" }
+        mutation CreateUserStatusMutation {
+          createUserStatus(
+          input: ${input}
           condition: {}
         ) {
           id
@@ -18,15 +20,15 @@ function handleUserApproved(args, whitelistAddress) {
   return query;
 }
 
-function handleAgreementSigned(args, whitelistAddress) {
-  const { user } = args;
-  // TODO find out where to store this informations
+function handleUserStatusUpdate(id, status = null, agreement = null) {
+  const input = status ? `{ id: "${id}", approved: ${Boolean(status)} }` :
+  `{ id: "${id}", agreementSigned: true }`
   const query = {
-    operationName: "UpdateWhitelist",
+    operationName: "UpdateUserStatusMutation",
     query: `
-        mutation UpdateWhitelist {
-          updateWhitelist(
-          input: { id: "${whitelistAddress}" }
+        mutation UpdateUserStatusMutation {
+          updateUserStatus(
+          input: ${input}
           condition: {}
         ) {
           id
@@ -36,6 +38,74 @@ function handleAgreementSigned(args, whitelistAddress) {
     variables: null,
   };
   return query;
+}
+
+async function handleUserApproved(args, whitelistAddress) {
+  const { _user, _status } = args;
+  const queryUserStatus = {
+    operationName: "UserStatusByWhitelistAndWalletAddress",
+    query: `
+        query UserStatusByWhitelistAndWalletAddress {
+          userStatusByWhitelistAndWalletAddress(
+          whitelistID: "${whitelistAddress}",
+          walletAddress: {eq: "${_user}"}
+        ) {
+          items {
+            id
+          }
+        }
+      }
+    `,
+    variables: null,
+  };
+   try {
+      const { body } = await poorMansGraphQL(queryUserStatus);
+      const result = JSON.parse(body);
+      const items = result.data.userStatusByWhitelistAndWalletAddress.items;
+      const userStatusExist = items.length > 0;
+      if (userStatusExist) {
+        return handleUserStatusUpdate(items[0].id, _status);
+      } else {
+        return handleUserStatusCreate(_user, whitelistAddress, _status);
+      }
+    } catch (error) {
+      console.log(error);
+      // silent error
+    }
+}
+
+async function handleAgreementSigned(args, whitelistAddress) {
+  const { _user } = args;
+  const queryUserStatus = {
+    operationName: "UserStatusByWhitelistAndWalletAddress",
+    query: `
+        query UserStatusByWhitelistAndWalletAddress {
+          userStatusByWhitelistAndWalletAddress(
+          whitelistID: "${whitelistAddress}",
+          walletAddress: {eq: "${_user}"}
+        ) {
+          items {
+            id
+          }
+        }
+      }
+    `,
+    variables: null,
+  };
+   try {
+      const { body } = await poorMansGraphQL(queryUserStatus);
+      const result = JSON.parse(body);
+      const items = result.data.userStatusByWhitelistAndWalletAddress.items;
+      const userStatusExist = items.length > 0;
+      if (userStatusExist) {
+        return handleUserStatusUpdate(items[0].id, null, true);
+      } else {
+        return handleUserStatusCreate(_user, whitelistAddress, null, true);
+      }
+    } catch (error) {
+      console.log(error);
+      // silent error
+    }
 }
 
 function handleWhitelistInitialised(args, whitelistAddress) {
@@ -46,7 +116,7 @@ function handleWhitelistInitialised(args, whitelistAddress) {
     query: `
         mutation UpdateWhitelist {
           updateWhitelist(
-          input: { id: ${whitelistAddress}, agreementHash: ${_agreementHash}, useApprovals: ${Boolean(_approvals)} }
+          input: { id: "${whitelistAddress}", agreementHash: "${_agreementHash}", useApprovals: ${Boolean(_approvals)} }
           condition: {}
         ) {
           id
--- index.js
+++ index.js
@@ -12,6 +12,30 @@ const WhitelistEvents = {
   'WhitelistInitialised': handleWhitelistInitialised
 }
 
+const fetchExistingWhitelists = async () => {
+  const query = {
+    operationName: "ListWhitelistsQuery",
+    query: `
+        query ListWhitelistsQuery {
+          listWhitelists {
+          items {
+            id
+          }
+        }
+      }
+    `,
+    variables: null,
+  };
+  try {
+    const { body } = await poorMansGraphQL(query);
+    const result = JSON.parse(body);
+    return result.data.listWhitelists.items;
+  } catch (error) {
+    console.log(error);
+    // silent error
+  }
+}
+
 const subsribeToWhitelist = async (whitelistAddress, provider) => {
   try {
     const contract = await new ethers.Contract(whitelistAddress, whitelist.abi, provider);
@@ -19,7 +43,7 @@ const subsribeToWhitelist = async (whitelistAddress, provider) => {
       const parsed = contract.interface.parseLog(event);
       const handler =  WhitelistEvents[event.event];
       if (!handler) return;
-      const query = handler(parsed.args, whitelistAddress);
+      const query = await handler(parsed.args, whitelistAddress);
       try {
         await poorMansGraphQL(query);
         output(`Database updated after event: ${event.event}`);
@@ -39,9 +63,12 @@ const subsribeToWhitelist = async (whitelistAddress, provider) => {
   const whitelistContracts = []; // Fetch from db and subsribe
 
   const coinmachineFactoryAddress = process.argv[2];
-
   try {
     const contract = await new ethers.Contract(coinmachineFactoryAddress, coinMachineFactory.abi, provider);
+    const existingWhitelists = await fetchExistingWhitelists();
+    await Promise.all((existingWhitelists || []).map(async (w) => {
+      await subsribeToWhitelist(w.id, provider);
+    }));
     contract.on('*', async(event) => {
       const parsed = contract.interface.parseLog(event);
       const { whitelist, owner } = parsed.args;
