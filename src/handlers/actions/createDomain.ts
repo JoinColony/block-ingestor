acccd4eef8df21ac8dd9f67de3eaf99fc3179c78
Jakub Zajac <jakub@colony.io>
2023-02-22 21:20:31 +0000

    Create domain in the db in CreateDomain action handler

--- src/handlers/actions/createDomain.ts
+++ src/handlers/actions/createDomain.ts
@@ -1,3 +1,5 @@
+import { mutate } from '~/amplifyClient';
+import networkClient from '~/networkClient';
 import { ColonyActionType, ContractEvent } from '~/types';
 import { toNumber, writeActionFromEvent } from '~/utils';
 import { getDatabaseDomainId } from '~/utils/domains';
@@ -6,10 +8,24 @@ export default async (event: ContractEvent): Promise<void> => {
   const { contractAddress: colonyAddress } = event;
   const { domainId, agent: initiatorAddress } = event.args;
   const nativeDomainId = toNumber(domainId);
+  const databaseDomainId = getDatabaseDomainId(colonyAddress, nativeDomainId);
 
-  writeActionFromEvent(event, colonyAddress, {
+  const colonyClient = await networkClient.getColonyClient(colonyAddress);
+  const [skillId, fundingPotId] = await colonyClient.getDomain(nativeDomainId);
+
+  await mutate('createDomain', {
+    input: {
+      id: databaseDomainId,
+      nativeId: nativeDomainId,
+      isRoot: false,
+      nativeFundingPotId: toNumber(fundingPotId),
+      nativeSkillId: toNumber(skillId),
+    },
+  });
+
+  await writeActionFromEvent(event, colonyAddress, {
     type: ColonyActionType.CreateDomain,
-    fromDomainId: getDatabaseDomainId(colonyAddress, nativeDomainId),
+    fromDomainId: databaseDomainId,
     initiatorAddress,
   });
 };
