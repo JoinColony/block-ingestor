2d9741e893e97ff49be524627a5ced5aace59499
Jakub Zajac <jakub@colony.io>
2023-02-28 20:42:51 +0000

    Cleanup: Remove old comment

--- src/handlers/actions/editDomain.ts
+++ src/handlers/actions/editDomain.ts
@@ -12,7 +12,6 @@ import {
   getDomainDatabaseId,
 } from '~/utils';
 
-// @TODO: Check if this really is EditDomain action as opposed to CreateDomain when created with some metadata
 export default async (event: ContractEvent): Promise<void> => {
   const receipt = await provider.getTransactionReceipt(event.transactionHash);
   const hasDomainAddedEvent = receipt.logs.some((log) =>
