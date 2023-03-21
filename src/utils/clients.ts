3f39fb1d7b1c027016754e507b3fb90fdfc33307
William Martin <williamajmartin@gmail.com>
2023-03-21 12:44:57 +0100

    refactor: add voting client utility

---
+++ src/utils/clients.ts
@@ -0,0 +1,9 @@
+import { AnyVotingReputationClient, Extension } from '@colony/colony-js';
+import networkClient from '../networkClient';
+
+export const getVotingClient = async (
+  colonyAddress: string,
+): Promise<AnyVotingReputationClient> => {
+  const colonyClient = await networkClient.getColonyClient(colonyAddress);
+  return await colonyClient.getExtensionClient(Extension.VotingReputation);
+};
