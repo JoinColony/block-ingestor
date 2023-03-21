aaa5bdb59dc97a5645f0d40c63dc4b12d29ae2b9
William Martin <williamajmartin@gmail.com>
2023-03-21 15:03:27 +0100

    feat: calculate updated users stakes

--- src/handlers/motions/motionStaked/helpers.ts
+++ src/handlers/motions/motionStaked/helpers.ts
@@ -1,5 +1,6 @@
 import { BigNumber } from 'ethers';
-import { MotionStakes } from '~types';
+import { MotionStakes, UserStakes } from '~types';
+import { getMotionSide } from '../helpers';
 
 export const getRequiredStake = (
   skillRep: BigNumber,
@@ -40,4 +41,98 @@ export const getRemainingStakes = (
   const remainingYayStake = requiredStake.sub(totalYayStakesRaw).toString();
   const remainingNayStake = requiredStake.sub(totalNayStakesRaw).toString();
   return [remainingNayStake, remainingYayStake];
+};
+
+const getStakePercentage = (
+  stake: BigNumber,
+  requiredStake: BigNumber,
+): BigNumber => stake.div(requiredStake).mul(100);
+
+const getUpdatedUserStakes = (
+  existingUserStakes: UserStakes,
+  amount: BigNumber,
+  vote: BigNumber,
+  requiredStake: BigNumber,
+): UserStakes => {
+  const stakedSide = getMotionSide(vote);
+  const existingRawStake = existingUserStakes.stakes.raw[stakedSide];
+  const updatedRawStake = amount.add(existingRawStake);
+  const updatedPercentage = getStakePercentage(updatedRawStake, requiredStake);
+  const updatedUserStakes: UserStakes = {
+    ...existingUserStakes,
+    stakes: {
+      raw: {
+        ...existingUserStakes.stakes.raw,
+        [stakedSide]: updatedRawStake,
+      },
+      percentage: {
+        ...existingUserStakes.stakes.percentage,
+        [stakedSide]: updatedPercentage,
+      },
+    },
+  };
+
+  return updatedUserStakes;
+};
+
+type UserStakesMapFn = (usersStakes: UserStakes) => UserStakes;
+
+const getUserStakesMapFn =
+  (
+    staker: string,
+    vote: BigNumber,
+    amount: BigNumber,
+    requiredStake: BigNumber,
+  ): UserStakesMapFn =>
+  (userStakes: UserStakes): UserStakes => {
+    const { address } = userStakes;
+
+    if (address === staker) {
+      return getUpdatedUserStakes(userStakes, amount, vote, requiredStake);
+    }
+
+    return userStakes;
+  };
+
+export const getUpdatedUsersStakes = (
+  usersStakes: UserStakes[],
+  staker: string,
+  vote: BigNumber,
+  amount: BigNumber,
+  requiredStake: BigNumber,
+): UserStakes[] => {
+  const isExistingStaker = usersStakes.some(
+    ({ address }) => address === staker,
+  );
+
+  if (isExistingStaker) {
+    const updateUserStakes = getUserStakesMapFn(
+      staker,
+      vote,
+      amount,
+      requiredStake,
+    );
+
+    return usersStakes.map(updateUserStakes);
+  }
+
+  const invertedVote = BigNumber.from(1).sub(vote);
+  const stakedSide = getMotionSide(vote);
+  const unstakedSide = getMotionSide(invertedVote);
+  const newUserStake = {
+    address: staker,
+    stakes: {
+      raw: {
+        [stakedSide]: amount.toString(),
+        [unstakedSide]: '0',
+      },
+
+      percentage: {
+        [stakedSide]: getStakePercentage(amount, requiredStake),
+        [unstakedSide]: '0',
+      },
+    }, // TS doesn't like the computed keys, but we know they're correct
+  } as unknown as UserStakes;
+
+  return [...usersStakes, newUserStake];
 };
