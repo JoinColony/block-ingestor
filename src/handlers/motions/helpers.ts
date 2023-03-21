db936f2b3ebbe9f49934dad06a7ede49a76747e3
William Martin <williamajmartin@gmail.com>
2023-03-21 15:12:10 +0100

    refactor: add updateMotionInDb helper

--- src/handlers/motions/helpers.ts
+++ src/handlers/motions/helpers.ts
@@ -1,7 +1,20 @@
 import { BigNumber } from 'ethers';
-import { MotionSide } from '~types';
+import { mutate } from '~amplifyClient';
+import { MotionData, MotionSide } from '~types';
 
 export * from './motionStaked/helpers';
 
 export const getMotionSide = (vote: BigNumber): MotionSide =>
   vote.eq(1) ? MotionSide.YAY : MotionSide.NAY;
+
+export const updateMotionInDB = async (
+  id: string,
+  motionData: MotionData,
+): Promise<void> => {
+  await mutate('updateColonyAction', {
+    input: {
+      id,
+      motionData,
+    },
+  });
+};
