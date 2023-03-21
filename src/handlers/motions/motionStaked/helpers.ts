03209947d6cc325bb56c2b1902318392c9953cbe
William Martin <williamajmartin@gmail.com>
2023-03-21 13:09:17 +0100

    refactor: move motion stakes calculations into helper

--- src/handlers/motions/motionStaked/helpers.ts
+++ src/handlers/motions/motionStaked/helpers.ts
@@ -1,7 +1,34 @@
 import { BigNumber } from 'ethers';
+import { MotionStakes } from '~types';
 
 export const getRequiredStake = (
   skillRep: BigNumber,
   totalStakeFraction: BigNumber,
 ): BigNumber =>
   skillRep.mul(totalStakeFraction).div(BigNumber.from(10).pow(18));
+
+export const getMotionStakes = (
+  requiredStake: BigNumber,
+  [totalNayStakesRaw, totalYayStakesRaw]: [BigNumber, BigNumber],
+): MotionStakes => {
+  const totalYayStakesPercentage = BigNumber.from(totalYayStakesRaw)
+    .div(requiredStake)
+    .mul(100);
+
+  const totalNayStakesPercentage = BigNumber.from(totalNayStakesRaw)
+    .div(requiredStake)
+    .mul(100);
+
+  const motionStakes: MotionStakes = {
+    raw: {
+      yay: totalYayStakesRaw.toString(),
+      nay: totalNayStakesRaw.toString(),
+    },
+    percentage: {
+      yay: totalYayStakesPercentage.toString(),
+      nay: totalNayStakesPercentage.toString(),
+    },
+  };
+
+  return motionStakes;
+};
