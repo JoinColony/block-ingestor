a7113c52b82df0af31a20166b087d82906e243b4
William Martin <williamajmartin@gmail.com>
2023-03-21 12:45:15 +0100

    feat: calculate required motion stake

--- src/handlers/motions/motionStaked/motionStaked.ts
+++ src/handlers/motions/motionStaked/motionStaked.ts
@@ -1,11 +1,19 @@
 import { ContractEvent } from '~types';
 import { verbose } from '~utils';
-import { getMotionSide } from '../helpers';
+import { getVotingClient } from '~utils/clients';
+import { getMotionSide, getRequiredStake } from '../helpers';
 
 export default async (event: ContractEvent): Promise<void> => {
   const {
+    contractAddress: colonyAddress,
     args: { vote, amount, staker, motionId },
   } = event;
+
+  const votingClient = await getVotingClient(colonyAddress);
+  const totalStakeFraction = await votingClient.getTotalStakeFraction();
+  const { skillRep } = await votingClient.getMotion(motionId);
+
+  const requiredStake = getRequiredStake(skillRep, totalStakeFraction);
 
   verbose(
     `User: ${staker} staked motion ${motionId} by ${amount.toString()} on side ${getMotionSide(
