38b86348acea32c2385894f2ab6c6cead87a13ad
William Martin <williamajmartin@gmail.com>
2023-04-05 22:11:20 +0200

    fix: motion finalized import

--- src/handlers/motions/index.ts
+++ src/handlers/motions/index.ts
@@ -1,3 +1,3 @@
 export { default as handleMotionCreated } from './motionCreated';
 export { default as handleMotionStaked } from './motionStaked';
-export { default as handleMotionFinalized } from './motionFinalized/motionFinalized';
+export { default as handleMotionFinalized } from './motionFinalized';
