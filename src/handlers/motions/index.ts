8e92266ffbe9695ff12143190831ff7369e13918
William Martin <williamajmartin@gmail.com>
2023-04-10 13:16:05 +0200

    feat: add motion vote revealed handler

--- src/handlers/motions/index.ts
+++ src/handlers/motions/index.ts
@@ -3,3 +3,4 @@ export { default as handleMotionStaked } from './motionStaked';
 export { default as handleMotionFinalized } from './motionFinalized';
 export { default as handleMotionRewardClaimed } from './motionRewardClaimed';
 export { default as handleMotionVoteSubmitted } from './motionVoteSubmitted';
+export { default as handleMotionVoteRevealed } from './motionVoteRevealed';
