05939bfda0e75918644e6c5cf63af7dd0164144a
William Martin <williamajmartin@gmail.com>
2023-04-17 11:20:52 +0200

    feat: add unlock token motion type

--- src/handlers/motions/motionCreated/handlers/index.ts
+++ src/handlers/motions/motionCreated/handlers/index.ts
@@ -1,3 +1,4 @@
 export { handleMintTokensMotion } from './mintTokens';
 export { handleNetworkUpgradeMotion } from './networkUpgrade';
 export { handleManageDomainMotion } from './manageDomain';
+export { handleUnlockTokenMotion } from './unlockToken';
