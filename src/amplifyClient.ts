36429053c39c1acd7bf2adfc1baffb2fd5c252ff
Juliana Halko <julianayasnog@gmail.com>
2023-04-18 13:33:52 -0400

    Refactor: use hasObjection flag to track objections

--- src/amplifyClient.ts
+++ src/amplifyClient.ts
@@ -211,6 +211,7 @@ export const queries = {
             }
             repSubmitted
             skillRep
+            hasObjection
             motionMessages {
               name
               messageKey
