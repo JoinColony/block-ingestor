de60568b626066303a2eb17f5f009aaae6c14fd5
William Martin <williamajmartin@gmail.com>
2023-04-18 18:30:31 +0200

    move: isSupportColonyClient to utils

--- src/handlers/actions/moveFunds.ts
+++ src/handlers/actions/moveFunds.ts
@@ -1,10 +1,3 @@
-import {
-  AnyColonyClient,
-  ColonyClientV1,
-  ColonyClientV2,
-  ColonyClientV3,
-  ColonyClientV4,
-} from '@colony/colony-js';
 import { BigNumber, utils } from 'ethers';
 
 import { ContractEvent, ContractEventsSignatures } from '~types';
@@ -14,23 +7,10 @@ import {
   getDomainDatabaseId,
   verbose,
   getCachedColonyClient,
+  isSupportedColonyClient,
 } from '~utils';
 import provider from '~provider';
 import { ColonyActionType } from '~graphql';
-
-/**
- * The handler makes use of colonyClient getDomainFromFundingPot method which is only
- * available on ColonyClientV5 and above. The following type predicate allows to check
- * we're dealing with a client that supports this method
- */
-type SupportedColonyClient = Exclude<
-  AnyColonyClient,
-  ColonyClientV1 | ColonyClientV2 | ColonyClientV3 | ColonyClientV4
->;
-const isSupportedColonyClient = (
-  colonyClient: AnyColonyClient,
-): colonyClient is SupportedColonyClient =>
-  (colonyClient as SupportedColonyClient).getDomainFromFundingPot !== undefined;
 
 export default async (event: ContractEvent): Promise<void> => {
   const receipt = await provider.getTransactionReceipt(event.transactionHash);
