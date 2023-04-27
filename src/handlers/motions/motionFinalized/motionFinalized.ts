ab6e38931e3312deeac5e8873b2c6575e54342ea
Armando Graterol <armandogrtrl@gmail.com>
2023-04-27 11:27:56 -0300

    Fixed lint errors in motionFinalized handler and added more comments

--- src/handlers/motions/motionFinalized/motionFinalized.ts
+++ src/handlers/motions/motionFinalized/motionFinalized.ts
@@ -8,7 +8,6 @@ import {
   getMotionFromDB,
   updateMotionInDB,
 } from '../helpers';
-
 import { getStakerReward, linkPendingDomainMetadataWithDomain } from './helpers';
 
 export default async (event: ContractEvent): Promise<void> => {
@@ -30,20 +29,26 @@ export default async (event: ContractEvent): Promise<void> => {
   );
   if (finalizedMotion) {
     const {
-      motionData: { usersStakes },
+      motionData: {
+        usersStakes,
+        revealedVotes: {
+          raw: { yay: yayVotes, nay: nayVotes },
+        },
+        motionStakes: {
+          percentage: { yay: yayPercentage, nay: nayPercentage },
+        },
+      },
       motionData,
     } = finalizedMotion;
 
-    const {
-      revealedVotes: {
-        raw: { yay: yayVotes, nay: nayVotes },
-      },
-      motionStakes: {
-        percentage: { yay: yayPercentage, nay: nayPercentage },
-      },
-    } = motionData;
     const yayWon = BigNumber.from(yayVotes).gt(nayVotes) || (Number(yayPercentage) > Number(nayPercentage));
 
+    /*
+     * pendingDomainMetadata is a motion data prop that we use to store the metadata of a Domain that COULD be created/edited
+     * if the YAY side of the motion won and the motion was finalized. In this step, if the motion has passed and has a pendingDomainMetadata prop,
+     * then we can assume that the motion's action is a domain action and we need to link this provisional DomainMetadata to the REAL Domain by creating
+     * a new DomainMetadata with the corresponding Domain item id.
+     */
     if (finalizedMotion.pendingDomainMetadata && yayWon) {
       await linkPendingDomainMetadataWithDomain(action, colonyAddress, finalizedMotion);
     }
