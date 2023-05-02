7273f629f3459be564b07d27ced73f14dfaa2770
Raul Glogovetan <raul@glogovetan.com>
2023-05-03 00:18:03 +0300

    Fix: networkClient set proper ENV network name

--- src/networkClient.ts
+++ src/networkClient.ts
@@ -6,7 +6,7 @@ import provider from './provider';
 dotenv.config();
 
 export default getColonyNetworkClient(
-  Network.Custom,
+  (process.env.CHAIN_NETWORK as Network) || Network.Custom,
   provider,
   { networkAddress: process.env.CHAIN_NETWORK_CONTRACT },
 );
