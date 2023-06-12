43abe4617c9da2bc0727041c66991b2dca0e3ae5
Jakub Zajac <jakub@colony.io>
2023-06-13 00:00:14 +0100

    Write stats even if PORT var not set

--- src/utils/stats.ts
+++ src/utils/stats.ts
@@ -28,16 +28,6 @@ type ObjectOrFunction =
 export const updateStats = async (
   objectOrFunction: ObjectOrFunction,
 ): Promise<void> => {
-  const port = process.env.STATS_PORT;
-  if (!port) {
-    verbose('Stats are DISABLED, no point in writing to the stats file');
-    /*
-     * If the port isn't set, it means the stats are disabled, so there's no point
-     * in keep writing the stats file, so we exit early
-     */
-    return;
-  }
-
   if (typeof objectOrFunction === 'function') {
     stats = {
       ...stats,
@@ -65,6 +55,10 @@ export const getStats = (): typeof stats => ({ ...stats });
 
 export const getLatestBlock = (): number =>
   Number.isInteger(stats.latestBlock) ? Number(stats.latestBlock) : 1;
+
+export const setLatestBlock = (latestBlock: number): void => {
+  updateStats({ latestBlock });
+};
 
 /**
  * Function fetching the last stored stats from the DB
