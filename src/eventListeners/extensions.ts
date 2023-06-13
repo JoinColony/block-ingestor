80b5fbbab1d84b065fd8e0f8b22387d8a5ca462c
Jakub Zajac <jakub@colony.io>
2023-06-13 21:53:25 +0100

    Setup listeners for existing extensions

--- src/eventListeners/extensions.ts
+++ src/eventListeners/extensions.ts
@@ -1,9 +1,56 @@
-import { addNetworkEventListener } from './network';
-import { ContractEventsSignatures } from '~types';
+import { ClientType, Extension, getExtensionHash } from '@colony/colony-js';
 
-export const setupListenersForExtensions = (): void => {
+import { ContractEventsSignatures } from '~types';
+import { query } from '~amplifyClient';
+import {
+  ListUninitializedExtensionsDocument,
+  ListUninitializedExtensionsQuery,
+  ListUninitializedExtensionsQueryVariables,
+} from '~graphql';
+import { notNull, output } from '~utils';
+import { addEventListener } from '~eventListeners';
+
+import { addNetworkEventListener } from './network';
+
+const fetchExtensionsAddresses = async (
+  extensionHash: string,
+): Promise<string[]> => {
+  const extensions = [];
+  let nextToken: string | undefined;
+
+  do {
+    const { data } =
+      (await query<
+        ListUninitializedExtensionsQuery,
+        ListUninitializedExtensionsQueryVariables
+      >(ListUninitializedExtensionsDocument, {
+        nextToken,
+        hash: extensionHash,
+      })) ?? {};
+
+    const { items } = data?.getExtensionsByHash ?? {};
+    extensions.push(...(items ?? []));
+
+    nextToken = data?.getExtensionsByHash?.nextToken ?? '';
+  } while (nextToken);
+
+  return extensions.filter(notNull).map((extension) => extension.id);
+};
+
+export const setupListenersForExtensions = async (): Promise<void> => {
   addNetworkEventListener(ContractEventsSignatures.ExtensionInstalled);
   addNetworkEventListener(ContractEventsSignatures.ExtensionUninstalled);
   addNetworkEventListener(ContractEventsSignatures.ExtensionDeprecated);
   addNetworkEventListener(ContractEventsSignatures.ExtensionUpgraded);
+
+  output(`Setting up listeners for VotingReputation extensions`);
+  const extensionHash = getExtensionHash(Extension.VotingReputation);
+  const addresses = await fetchExtensionsAddresses(extensionHash);
+  addresses.forEach((extensionAddress) => {
+    addEventListener({
+      eventSignature: ContractEventsSignatures.ExtensionInitialised,
+      address: extensionAddress,
+      clientType: ClientType.VotingReputationClient,
+    });
+  });
 };
