2a4b828fa1c1506e2b41c5423f3ac96783663c0a
Jakub Zajac <jakub@colony.io>
2023-06-21 23:49:11 +0100

    Feat: Get ABI interface for token clients

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -3,11 +3,16 @@ import { utils, constants } from 'ethers';
 import {
   IColonyEvents__factory as ColonyEventsFactory,
   VotingReputationEvents__factory as VotingReputationEventsFactory,
+  TokenEvents__factory as TokenEventsFactory,
 } from '@colony/events';
 
 import networkClient from '~networkClient';
 import provider from '~provider';
 
+/**
+ * Function returning ABI-derived interface for a given client type,
+ * which is later used for parsing event logs
+ */
 export const getInterfaceByClientType = (
   clientType: ClientType,
 ): utils.Interface | null => {
@@ -24,6 +29,10 @@ export const getInterfaceByClientType = (
         constants.AddressZero,
         provider,
       ).interface;
+    }
+    case ClientType.TokenClient: {
+      return TokenEventsFactory.connect(constants.AddressZero, provider)
+        .interface;
     }
     default: {
       return null;
