10c7c471a00ac8b63b7d6db727865ad8bb100e13
William Martin <williamajmartin@gmail.com>
2023-07-03 16:24:27 +0200

    fix: claim not showing when transferring between colonies

--- src/handlers/actions/payment.ts
+++ src/handlers/actions/payment.ts
@@ -1,7 +1,14 @@
 import { utils } from 'ethers';
-import { ColonyActionType } from '~graphql';
+import { mutate } from '~amplifyClient';
+import {
+  ColonyActionType,
+  CreateColonyFundsClaimDocument,
+  CreateColonyFundsClaimMutation,
+  CreateColonyFundsClaimMutationVariables,
+} from '~graphql';
+import networkClient from '~networkClient';
 
-import provider from '~provider';
+import provider, { getChainId } from '~provider';
 import { ContractEvent, ContractEventsSignatures } from '~types';
 import {
   writeActionFromEvent,
@@ -21,7 +28,12 @@ const events = [
 const hashedSignatures = events.map((signature) => utils.id(signature));
 
 export default async (paymentAddedEvent: ContractEvent): Promise<void> => {
-  const { contractAddress: colonyAddress, transactionHash } = paymentAddedEvent;
+  const {
+    contractAddress: colonyAddress,
+    transactionHash,
+    logIndex,
+    blockNumber,
+  } = paymentAddedEvent;
 
   const colonyClient = await getCachedColonyClient(colonyAddress);
 
@@ -53,8 +65,29 @@ export default async (paymentAddedEvent: ContractEvent): Promise<void> => {
   const { paymentId } = paymentAddedEvent.args;
   const { recipient: recipientAddress, domainId } =
     await colonyClient.getPayment(paymentId);
+
+  const recipientIsColony = await networkClient.isColony(recipientAddress);
+
   const { token: tokenAddress, amount } = payoutClaimedEvent.args;
   const [initiatorAddress, fundamentalChainId] = oneTxPaymentMadeEvent.args;
+
+  if (recipientIsColony) {
+    const chainId = getChainId();
+    const claimId = `${chainId}_${transactionHash}_${logIndex}`;
+
+    await mutate<
+      CreateColonyFundsClaimMutation,
+      CreateColonyFundsClaimMutationVariables
+    >(CreateColonyFundsClaimDocument, {
+      input: {
+        id: claimId,
+        colonyFundsClaimsId: recipientAddress,
+        colonyFundsClaimTokenId: tokenAddress,
+        createdAtBlock: blockNumber,
+        amount: amount.toString(),
+      },
+    });
+  }
 
   await writeActionFromEvent(paymentAddedEvent, colonyAddress, {
     type: ColonyActionType.Payment,
