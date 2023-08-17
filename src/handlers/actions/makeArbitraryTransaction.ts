9c9b737a7b16718e99ef423df1c6a22d268c5f55
Armando Graterol <armandogrtrl@gmail.com>
2023-08-17 20:20:55 -0300

    Updated initiatorAddress of ArbitraryTransaction event

--- src/handlers/actions/makeArbitraryTransaction.ts
+++ src/handlers/actions/makeArbitraryTransaction.ts
@@ -1,14 +1,16 @@
 import { ColonyActionType } from '~graphql';
 import { ContractEvent } from '~types';
+import provider from '~provider';
 import { writeActionFromEvent } from '~utils';
 
 export default async (event: ContractEvent): Promise<void> => {
   const { contractAddress: colonyAddress } = event;
   const { target: recipientAddress } = event.args;
+  const receipt = await provider.getTransactionReceipt(event.transactionHash);
 
   await writeActionFromEvent(event, colonyAddress, {
     type: ColonyActionType.MakeArbitraryTransaction,
-    initiatorAddress: colonyAddress,
+    initiatorAddress: receipt.from,
     recipientAddress,
   });
 };
