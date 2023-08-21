562a1e812f8826feceab016205cdd29ae2029f21
Jakub Zajac <jakub@colony.io>
2023-08-21 21:22:08 +0100

    Setup listeners for existing Staked Expenditure installations

--- src/eventListeners/stakedExpenditure.ts
+++ src/eventListeners/stakedExpenditure.ts
@@ -1,7 +1,16 @@
 import { utils } from 'ethers';
+import { Extension, getExtensionHash } from '@colony/colony-js';
 
 import { ContractEventsSignatures } from '~types';
 import { EventListenerType, addEventListener } from '~eventListeners';
+import { notNull, output } from '~utils';
+import {
+  ExtensionFragment,
+  ListExtensionsDocument,
+  ListExtensionsQuery,
+  ListExtensionsQueryVariables,
+} from '~graphql';
+import { query } from '~amplifyClient';
 
 export const addStakedExpenditureEventListener = (
   eventSignature: ContractEventsSignatures,
@@ -15,6 +24,44 @@ export const addStakedExpenditureEventListener = (
     colonyAddress,
     topics: [utils.id(eventSignature)],
   });
+};
+
+export const setupListenersForStakedExpenditureExtensions =
+  async (): Promise<void> => {
+    output(`Setting up listeners for StakedExpenditure extensions`);
+    const existingExtensions = await fetchExistingStakedExpenditureExtensions();
+    existingExtensions.forEach((extension) =>
+      setupListenersForStakedExpenditure(
+        extension.id,
+        extension.colonyId,
+        extension.isInitialized,
+      ),
+    );
+  };
+
+const fetchExistingStakedExpenditureExtensions = async (): Promise<
+  ExtensionFragment[]
+> => {
+  const extensions = [];
+  let nextToken: string | undefined;
+
+  do {
+    const { data } =
+      (await query<ListExtensionsQuery, ListExtensionsQueryVariables>(
+        ListExtensionsDocument,
+        {
+          nextToken,
+          hash: getExtensionHash(Extension.StakedExpenditure),
+        },
+      )) ?? {};
+
+    const { items } = data?.getExtensionsByHash ?? {};
+    extensions.push(...(items ?? []));
+
+    nextToken = data?.getExtensionsByHash?.nextToken ?? '';
+  } while (nextToken);
+
+  return extensions.filter(notNull);
 };
 
 export const setupListenersForStakedExpenditure = (
