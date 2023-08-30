0b3857fd472c61624e149daaa2992656f79bc0b8
Armando Graterol <armandogrtrl@gmail.com>
2023-08-30 16:08:19 -0300

    Added fromDomainId to makeArbitraryTransaction motion

--- src/handlers/motions/motionCreated/handlers/makeArbitraryTransactions.ts
+++ src/handlers/motions/motionCreated/handlers/makeArbitraryTransactions.ts
@@ -1,18 +1,27 @@
 import { TransactionDescription } from 'ethers/lib/utils';
+import { Id } from '@colony/colony-js';
 
 import { ContractEvent, motionNameMapping } from '~types';
 
 import { createMotionInDB } from '../helpers';
+import { getDomainDatabaseId } from '~utils';
 
 export const handleMakeArbitraryTransactionsMotion = async (
   event: ContractEvent,
   parsedAction: TransactionDescription,
 ): Promise<void> => {
+  const { colonyAddress } = event;
+
+  if (!colonyAddress) {
+    return;
+  }
+
   const { name, args: actionArgs } = parsedAction;
   const [recipients] = actionArgs;
 
   await createMotionInDB(event, {
     type: motionNameMapping[name],
     recipientAddress: recipients[0],
+    fromDomainId: getDomainDatabaseId(colonyAddress, Id.RootDomain),
   });
 };
