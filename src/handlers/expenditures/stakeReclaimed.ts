70c5117ddf6f8f9e8af6932978ed4ff7f1ef4855
Jakub Zajac <jakub@colony.io>
2023-08-31 16:57:33 +0100

    Fix: StakeReclaimed event listener

--- src/handlers/expenditures/stakeReclaimed.ts
+++ src/handlers/expenditures/stakeReclaimed.ts
@@ -8,9 +8,13 @@ import { ContractEvent } from '~types';
 import { getExpenditureDatabaseId, toNumber, verbose } from '~utils';
 
 export default async (event: ContractEvent): Promise<void> => {
-  const { contractAddress: colonyAddress } = event;
+  const { colonyAddress } = event;
   const { expenditureId } = event.args;
   const convertedExpenditureId = toNumber(expenditureId);
+
+  if (!colonyAddress) {
+    return;
+  }
 
   verbose(
     `Stake reclaimed for expenditure with ID ${convertedExpenditureId} in colony ${colonyAddress}`,
