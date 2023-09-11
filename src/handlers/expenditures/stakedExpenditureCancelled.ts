12600a34cf3bbc2311bdcd29fbe7ce490a5a2d48
Jakub Zajac <jakub@colony.io>
2023-09-12 00:15:18 +0100

    Handle staked expenditure cancelled event

--- src/handlers/expenditures/stakedExpenditureCancelled.ts
+++ src/handlers/expenditures/stakedExpenditureCancelled.ts
@@ -3,7 +3,11 @@ import { ContractEvent } from '~types';
 import { updateCancelledExpenditure } from './helpers';
 
 export default async (event: ContractEvent): Promise<void> => {
-  const { contractAddress: colonyAddress } = event;
+  const { colonyAddress } = event;
+
+  if (!colonyAddress) {
+    return;
+  }
 
   await updateCancelledExpenditure(colonyAddress, event);
 };
