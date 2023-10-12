b295097d3eb1f40611310add5793f57dd98e0120
Jakub Zajac <jakub@colony.io>
2023-10-13 00:12:15 +0100

    Decode updated recipients in expenditureStateChanged handler

--- src/handlers/expenditures/expenditureStateChanged.ts
+++ src/handlers/expenditures/expenditureStateChanged.ts
@@ -1,5 +1,71 @@
+import { BigNumber, BigNumberish, ethers } from 'ethers';
 import { ContractEvent } from '~types';
+import { getExpenditureFromDB } from './helpers';
+import { getExpenditureDatabaseId, output, toNumber } from '~utils';
+import { mutate } from '~amplifyClient';
+import {
+  ExpenditureSlot,
+  UpdateExpenditureDocument,
+  UpdateExpenditureMutation,
+  UpdateExpenditureMutationVariables,
+} from '~graphql';
+
+const toB32 = (input: BigNumberish): string =>
+  ethers.utils.hexZeroPad(ethers.utils.hexlify(input), 32);
+
+const EXPENDITURESLOTS_SLOT = BigNumber.from(26);
+
+const EXPENDITURESLOT_RECIPIENT = toB32(ethers.BigNumber.from(0));
 
 export default async (event: ContractEvent): Promise<void> => {
-  console.log(event);
+  const { contractAddress: colonyAddress } = event;
+  const { storageSlot, value, expenditureId } = event.args;
+  // The unfortunate naming of the `keys` property means we have to access it like so
+  const keys = event.args[4];
+  const convertedExpenditureId = toNumber(expenditureId);
+
+  const databaseId = getExpenditureDatabaseId(
+    colonyAddress,
+    convertedExpenditureId,
+  );
+
+  const expenditure = await getExpenditureFromDB(databaseId);
+  if (!expenditure) {
+    output(
+      `Could not find expenditure with ID: ${databaseId} in the db. This is a bug and needs investigating.`,
+    );
+    return;
+  }
+
+  let updatedSlots: ExpenditureSlot[] | undefined;
+
+  if (storageSlot.eq(EXPENDITURESLOTS_SLOT)) {
+    if (keys[1] === EXPENDITURESLOT_RECIPIENT) {
+      const slotId = ethers.BigNumber.from(keys[0]).toNumber();
+      const recipientAddress = ethers.utils.defaultAbiCoder
+        .decode(['address'], value)
+        .toString();
+
+      const existingSlot = expenditure.slots.find((slot) => slot.id === slotId);
+      const updatedSlot: ExpenditureSlot = {
+        ...existingSlot,
+        id: slotId,
+        recipientAddress,
+      };
+      updatedSlots = [
+        ...expenditure.slots.filter((slot) => slot.id !== slotId),
+        updatedSlot,
+      ];
+    }
+  }
+
+  await mutate<UpdateExpenditureMutation, UpdateExpenditureMutationVariables>(
+    UpdateExpenditureDocument,
+    {
+      input: {
+        id: databaseId,
+        slots: updatedSlots,
+      },
+    },
+  );
 };
