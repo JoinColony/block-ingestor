b9f79bbc2f72971a61b856187fef153796124972
Bassgeta <kerry@colony.io>
2024-02-06 11:59:11 +0100

    refactor: parsing of metadata delta operation, naming of operations

--- src/handlers/metadataDelta/utils.ts
+++ src/handlers/metadataDelta/utils.ts
@@ -1,24 +1,39 @@
 import { verbose } from '~utils';
-import { MetadataDeltaOperation, MetadataDeltaActionType } from './types';
+import { MetadataDeltaOperation, MetadataDeltaOperationType } from './types';
 
-export const isMetadataDeltaOperation = (
+const isMetadataDeltaOperation = (
   operation: any,
 ): operation is MetadataDeltaOperation => {
   return (
     typeof operation === 'object' &&
     operation !== null &&
     operation.type !== undefined &&
-    Object.values(MetadataDeltaActionType).includes(operation.type)
+    Object.values(MetadataDeltaOperationType).includes(operation.type)
   );
 };
 
-export const parseOperation = (operationString: string): object | null => {
-  const operation = JSON.parse(operationString);
+export const parseOperation = (
+  operationString: string,
+): MetadataDeltaOperation | null => {
+  try {
+    const operation = JSON.parse(operationString);
 
-  if (typeof operation !== 'object') {
-    verbose('Operation not an object: ', operation);
+    if (typeof operation !== 'object') {
+      verbose('Operation not an object: ', operation);
+      return null;
+    }
+
+    if (!isMetadataDeltaOperation(operation)) {
+      verbose(
+        'Operation does not conform to MetadataDeltaOperation type: ',
+        operation,
+      );
+      return null;
+    }
+
+    return operation;
+  } catch (error) {
+    verbose(`String: ${operationString} cannot be parsed`, error);
     return null;
   }
-
-  return operation;
 };
