6c6c612faeef32acddd77a1639292bc1896b7be5
Jakub Zajac <jakub@colony.io>
2024-02-20 23:20:34 +0000

    Feat: update user stake when expenditure stake is reclaimed

--- src/handlers/expenditures/stakeReclaimed.ts
+++ src/handlers/expenditures/stakeReclaimed.ts
@@ -3,9 +3,13 @@ import {
   UpdateExpenditureDocument,
   UpdateExpenditureMutation,
   UpdateExpenditureMutationVariables,
+  UpdateUserStakeDocument,
+  UpdateUserStakeMutation,
+  UpdateUserStakeMutationVariables,
 } from '~graphql';
 import { ContractEvent } from '~types';
 import { getExpenditureDatabaseId, toNumber, verbose } from '~utils';
+import { getUserStakeDatabaseId } from '~utils/stakes';
 
 export default async (event: ContractEvent): Promise<void> => {
   const { colonyAddress } = event;
@@ -16,17 +20,37 @@ export default async (event: ContractEvent): Promise<void> => {
     return;
   }
 
-  verbose(
-    `Stake reclaimed for expenditure with ID ${convertedExpenditureId} in colony ${colonyAddress}`,
+  const databaseId = getExpenditureDatabaseId(
+    colonyAddress,
+    convertedExpenditureId,
   );
 
-  mutate<UpdateExpenditureMutation, UpdateExpenditureMutationVariables>(
-    UpdateExpenditureDocument,
-    {
-      input: {
-        id: getExpenditureDatabaseId(colonyAddress, convertedExpenditureId),
-        hasReclaimedStake: true,
-      },
+  const updateExpenditureResponse = await mutate<
+    UpdateExpenditureMutation,
+    UpdateExpenditureMutationVariables
+  >(UpdateExpenditureDocument, {
+    input: {
+      id: databaseId,
+      hasReclaimedStake: true,
     },
+  });
+  const expenditureDetails = updateExpenditureResponse?.data?.updateExpenditure;
+
+  if (expenditureDetails?.stakedTransactionHash) {
+    const { ownerAddress, stakedTransactionHash } = expenditureDetails;
+
+    await mutate<UpdateUserStakeMutation, UpdateUserStakeMutationVariables>(
+      UpdateUserStakeDocument,
+      {
+        input: {
+          id: getUserStakeDatabaseId(ownerAddress, stakedTransactionHash),
+          isClaimed: true,
+        },
+      },
+    );
+  }
+
+  verbose(
+    `Stake reclaimed for expenditure with ID ${convertedExpenditureId} in colony ${colonyAddress}`,
   );
 };
