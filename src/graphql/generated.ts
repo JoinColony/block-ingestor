e0eeef7149cf05adf0a619cf1985110c9398060b
Jakub Zajac <jakub@colony.io>
2024-02-21 00:04:09 +0000

    Feat: Update user stake when staked expenditure is cancelled

--- src/graphql/generated.ts
+++ src/graphql/generated.ts
@@ -1520,6 +1520,7 @@ export type CreateUserStakeInput = {
   createdAt?: InputMaybe<Scalars['AWSDateTime']>;
   id?: InputMaybe<Scalars['ID']>;
   isClaimed: Scalars['Boolean'];
+  isForfeited?: InputMaybe<Scalars['Boolean']>;
   userAddress: Scalars['ID'];
 };
 
@@ -1877,7 +1878,7 @@ export type Expenditure = {
   ownerAddress: Scalars['ID'];
   /** Array containing expenditure slots */
   slots: Array<ExpenditureSlot>;
-  /** Hash of the `ExpenditureMadeViaStake` transaction, if applicable */
+  /** Hash of the `ExpenditureMadeViaStake` event transaction, if applicable */
   stakedTransactionHash?: Maybe<Scalars['ID']>;
   /** Status of the expenditure */
   status: ExpenditureStatus;
@@ -4002,6 +4003,7 @@ export type ModelSubscriptionUserStakeFilterInput = {
   createdAt?: InputMaybe<ModelSubscriptionStringInput>;
   id?: InputMaybe<ModelSubscriptionIdInput>;
   isClaimed?: InputMaybe<ModelSubscriptionBooleanInput>;
+  isForfeited?: InputMaybe<ModelSubscriptionBooleanInput>;
   or?: InputMaybe<Array<InputMaybe<ModelSubscriptionUserStakeFilterInput>>>;
   userAddress?: InputMaybe<ModelSubscriptionIdInput>;
 };
@@ -4159,6 +4161,7 @@ export type ModelUserStakeConditionInput = {
   colonyAddress?: InputMaybe<ModelIdInput>;
   createdAt?: InputMaybe<ModelStringInput>;
   isClaimed?: InputMaybe<ModelBooleanInput>;
+  isForfeited?: InputMaybe<ModelBooleanInput>;
   not?: InputMaybe<ModelUserStakeConditionInput>;
   or?: InputMaybe<Array<InputMaybe<ModelUserStakeConditionInput>>>;
   userAddress?: InputMaybe<ModelIdInput>;
@@ -4178,6 +4181,7 @@ export type ModelUserStakeFilterInput = {
   createdAt?: InputMaybe<ModelStringInput>;
   id?: InputMaybe<ModelIdInput>;
   isClaimed?: InputMaybe<ModelBooleanInput>;
+  isForfeited?: InputMaybe<ModelBooleanInput>;
   not?: InputMaybe<ModelUserStakeFilterInput>;
   or?: InputMaybe<Array<InputMaybe<ModelUserStakeFilterInput>>>;
   userAddress?: InputMaybe<ModelIdInput>;
@@ -7902,6 +7906,7 @@ export type UpdateUserStakeInput = {
   createdAt?: InputMaybe<Scalars['AWSDateTime']>;
   id: Scalars['ID'];
   isClaimed?: InputMaybe<Scalars['Boolean']>;
+  isForfeited?: InputMaybe<Scalars['Boolean']>;
   userAddress?: InputMaybe<Scalars['ID']>;
 };
 
@@ -7993,6 +7998,8 @@ export type UserStake = {
   /** Self-managed, formatted as userAddress_transactionHash, where transactionHash is the hash of the transaction that is being staked for */
   id: Scalars['ID'];
   isClaimed: Scalars['Boolean'];
+  /** Only applicable for expenditure stakes, indicates if the creator's stake was forfeited when expenditure was cancelled */
+  isForfeited?: Maybe<Scalars['Boolean']>;
   updatedAt: Scalars['AWSDateTime'];
   user: User;
   userAddress: Scalars['ID'];
--- src/handlers/expenditures/expenditureStakerPunished.ts
+++ src/handlers/expenditures/expenditureStakerPunished.ts
@@ -3,9 +3,13 @@ import {
   UpdateExpenditureDocument,
   UpdateExpenditureMutation,
   UpdateExpenditureMutationVariables,
+  UpdateUserStakeDocument,
+  UpdateUserStakeMutation,
+  UpdateUserStakeMutationVariables,
 } from '~graphql';
 import { ContractEvent } from '~types';
 import { getExpenditureDatabaseId, toNumber, verbose } from '~utils';
+import { getUserStakeDatabaseId } from '~utils/stakes';
 
 export default async (event: ContractEvent): Promise<void> => {
   const { colonyAddress } = event;
@@ -27,13 +31,28 @@ export default async (event: ContractEvent): Promise<void> => {
     }punished`,
   );
 
-  await mutate<UpdateExpenditureMutation, UpdateExpenditureMutationVariables>(
-    UpdateExpenditureDocument,
-    {
-      input: {
-        id: databaseId,
-        isStakeForfeited: punished,
-      },
+  const updateExpenditureResponse = await mutate<
+    UpdateExpenditureMutation,
+    UpdateExpenditureMutationVariables
+  >(UpdateExpenditureDocument, {
+    input: {
+      id: databaseId,
+      isStakeForfeited: punished,
     },
-  );
+  });
+  const expenditureDetails = updateExpenditureResponse?.data?.updateExpenditure;
+
+  if (expenditureDetails?.stakedTransactionHash) {
+    const { ownerAddress, stakedTransactionHash } = expenditureDetails;
+
+    await mutate<UpdateUserStakeMutation, UpdateUserStakeMutationVariables>(
+      UpdateUserStakeDocument,
+      {
+        input: {
+          id: getUserStakeDatabaseId(ownerAddress, stakedTransactionHash),
+          isForfeited: punished,
+        },
+      },
+    );
+  }
 };
