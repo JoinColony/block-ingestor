c8f274a3b38f9c4645da4d9e3336d94e927a1415
Jakub Zajac <jakub@colony.io>
2024-02-28 22:14:37 +0000

    Add `Expenditure` GraphQL fragment

--- src/graphql/generated.ts
+++ src/graphql/generated.ts
@@ -8169,6 +8169,32 @@ export type ExpenditureBalanceFragment = {
   amount: string;
 };
 
+export type ExpenditureFragment = {
+  __typename?: 'Expenditure';
+  id: string;
+  stakedTransactionHash?: string | null;
+  ownerAddress: string;
+  slots: Array<{
+    __typename?: 'ExpenditureSlot';
+    id: number;
+    recipientAddress?: string | null;
+    claimDelay?: number | null;
+    payoutModifier?: number | null;
+    payouts?: Array<{
+      __typename?: 'ExpenditurePayout';
+      tokenAddress: string;
+      amount: string;
+      isClaimed: boolean;
+      networkFee?: string | null;
+    }> | null;
+  }>;
+  balances?: Array<{
+    __typename?: 'ExpenditureBalance';
+    tokenAddress: string;
+    amount: string;
+  }> | null;
+};
+
 export type ExtensionFragment = {
   __typename?: 'ColonyExtension';
   id: string;
@@ -9479,6 +9505,29 @@ export const ExpenditureBalance = gql`
     amount
   }
 `;
+export const Expenditure = gql`
+  fragment Expenditure on Expenditure {
+    id
+    slots {
+      id
+      recipientAddress
+      claimDelay
+      payoutModifier
+      payouts {
+        tokenAddress
+        amount
+        isClaimed
+        networkFee
+      }
+    }
+    balances {
+      ...ExpenditureBalance
+    }
+    stakedTransactionHash
+    ownerAddress
+  }
+  ${ExpenditureBalance}
+`;
 export const Extension = gql`
   fragment Extension on ColonyExtension {
     id
@@ -10053,27 +10102,10 @@ export const GetContractEventDocument = gql`
 export const GetExpenditureDocument = gql`
   query GetExpenditure($id: ID!) {
     getExpenditure(id: $id) {
-      id
-      slots {
-        id
-        recipientAddress
-        claimDelay
-        payoutModifier
-        payouts {
-          tokenAddress
-          amount
-          isClaimed
-          networkFee
-        }
-      }
-      balances {
-        ...ExpenditureBalance
-      }
-      stakedTransactionHash
-      ownerAddress
+      ...Expenditure
     }
   }
-  ${ExpenditureBalance}
+  ${Expenditure}
 `;
 export const GetExpenditureByNativeFundingPotIdAndColonyDocument = gql`
   query GetExpenditureByNativeFundingPotIdAndColony(
