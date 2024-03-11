3296229fec49f2e8dc39d09b4dbfd885b1414f48
Sam Gibbs <34915414+iamsamgibbs@users.noreply.github.com>
2024-03-11 10:59:30 +0000

    Fix: Refactor multicall handlers

--- src/handlers/motions/motionCreated/handlers/multicall/fragments.ts
+++ src/handlers/motions/motionCreated/handlers/multicall/fragments.ts
@@ -1,3 +1,42 @@
+import { ContractEvent } from '~types';
+import { DecodedFunctions } from './multicall';
+import { ExpenditureFragment, ExpenditureStatus } from '~graphql';
+import {
+  editLockedExpenditureMotionHandler,
+  fundExpenditureMotionHandler,
+  isEditLockedExpenditureMotion,
+  isFundExpenditureMotion,
+  isReleaseExpenditureStageMotion,
+  releaseExpenditureStageMotionHandler,
+} from './multicallHandlers';
+
+export type MulticallHandler = ({
+  event,
+  decodedFunctions,
+  gasEstimate,
+  expenditure,
+}: {
+  event: ContractEvent;
+  gasEstimate: string;
+  decodedFunctions: DecodedFunctions;
+  expenditure?: ExpenditureFragment;
+}) => void | Promise<void>;
+
+export type MulticallValidator = ({
+  decodedFunctions,
+  expenditureStatus,
+}: {
+  decodedFunctions: DecodedFunctions;
+  expenditureStatus?: ExpenditureStatus;
+}) => boolean;
+
+export const multicallHandlers: Array<[MulticallValidator, MulticallHandler]> =
+  [
+    [isFundExpenditureMotion, fundExpenditureMotionHandler],
+    [isReleaseExpenditureStageMotion, releaseExpenditureStageMotionHandler],
+    [isEditLockedExpenditureMotion, editLockedExpenditureMotionHandler],
+  ];
+
 // List all supported multicall fragments
 export const supportedMulticallFragments: string[] = [
   'moveFundsBetweenPots(uint256,uint256,uint256,uint256,uint256,uint256,address)',
