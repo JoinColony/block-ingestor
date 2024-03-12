7eb3ab37df665486d25055e7beedb2d6e75d3644
Sam Gibbs <34915414+iamsamgibbs@users.noreply.github.com>
2024-03-12 10:41:19 +0000

    Fix: Move toB32 util to numbers util

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,6 @@
 import { Extension } from '@colony/colony-js';
-import { BigNumber, BigNumberish, utils } from 'ethers';
+import { BigNumber } from 'ethers';
+import { toB32 } from '~utils';
 
 export const COLONY_CURRENT_VERSION_KEY = 'colony';
 export const NETWORK_INVERSE_FEE_DATABASE_ID = 'networkInverseFee';
@@ -13,9 +14,6 @@ export const SUPPORTED_EXTENSION_IDS = [
 ];
 
 export const SIMPLE_DECISIONS_ACTION_CODE = '0x12345678';
-
-const toB32 = (input: BigNumberish): string =>
-  utils.hexZeroPad(utils.hexlify(input), 32);
 
 export const EXPENDITURES_SLOT = BigNumber.from(25);
 export const EXPENDITURESLOTS_SLOT = BigNumber.from(26);
--- src/utils/numbers.ts
+++ src/utils/numbers.ts
@@ -1,4 +1,7 @@
-import { BigNumber, BigNumberish } from 'ethers';
+import { BigNumber, BigNumberish, utils } from 'ethers';
 
 export const toNumber = (bigNumber: BigNumberish): number =>
   BigNumber.from(bigNumber).toNumber();
+
+export const toB32 = (input: BigNumberish): string =>
+  utils.hexZeroPad(utils.hexlify(input), 32);
