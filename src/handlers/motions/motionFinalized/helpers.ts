ac72d5490ba7f466a0bcfe70bc9d146ab068ab03
Sam Gibbs <34915414+iamsamgibbs@users.noreply.github.com>
2024-03-12 11:59:11 +0000

    Fix: ContractMethodSignatures typo

--- src/handlers/motions/motionFinalized/helpers.ts
+++ src/handlers/motions/motionFinalized/helpers.ts
@@ -415,7 +415,7 @@ export const claimExpenditurePayouts = async (
 
   try {
     decodedSetExpenditureStateArgs = colonyClient.interface.decodeFunctionData(
-      ContractMethodSignatures.setExpenditureState,
+      ContractMethodSignatures.SetExpenditureState,
       firstAction,
     );
   } catch (error) {
