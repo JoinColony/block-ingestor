7276ea1e40f7017c4ad2060dba5abd1358d77c46
Jakub Zajac <jakub@colony.io>
2024-03-21 20:46:28 +0000

    Cleanup: Remove unused code

--- src/utils/extensions/contracts.ts
+++
@@ -1,9 +0,0 @@
-import { Contract } from 'ethers';
-
-import { ContractEventsSignatures } from '~types';
-
-/**
- * Function returning OneTxPayment contract, currently used for handling the Payment action
- */
-export const getOneTxPaymentContract = (address: string): Contract =>
-  new Contract(address, [`event ${ContractEventsSignatures.OneTxPaymentMade}`]);
