bdc9959887378fedc5a062edcf7252e9bafebe89
Jakub Zajac <jakub@colony.io>
2024-03-27 09:50:50 +0000

    Feat: Handle motions finalizing an expenditure

--- src/handlers/motions/motionCreated/motionCreated.ts
+++ src/handlers/motions/motionCreated/motionCreated.ts
@@ -28,6 +28,7 @@ import {
   handleCancelStakedExpenditureMotion,
   handleMetadataDeltaMotion,
   handleCancelExpenditureViaArbitrationMotion,
+  handleFinalizeExpenditureViaArbitrationMotion,
 } from './handlers';
 
 export default async (event: ContractEvent): Promise<void> => {
@@ -224,6 +225,15 @@ export default async (event: ContractEvent): Promise<void> => {
 
       case ColonyOperations.CancelExpenditureViaArbitration: {
         await handleCancelExpenditureViaArbitrationMotion(
+          event,
+          parsedAction,
+          gasEstimate,
+        );
+        break;
+      }
+
+      case ColonyOperations.FinalizeExpenditureViaArbitration: {
+        await handleFinalizeExpenditureViaArbitrationMotion(
           event,
           parsedAction,
           gasEstimate,
