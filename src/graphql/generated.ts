f1b3a947167f1541bf6c648ed0a8811afde68aae
Jakub Zajac <jakub@colony.io>
2024-04-05 11:39:58 +0100

    Fix: Do not overwrite colony current version with an older one

--- src/graphql/generated.ts
+++ src/graphql/generated.ts
@@ -8986,6 +8986,18 @@ export type GetColonyContributorQuery = {
   } | null;
 };
 
+export type GetCurrentColonyVersionQueryVariables = Exact<{
+  [key: string]: never;
+}>;
+
+export type GetCurrentColonyVersionQuery = {
+  __typename?: 'Query';
+  getCurrentVersionByKey?: {
+    __typename?: 'ModelCurrentVersionConnection';
+    items: Array<{ __typename?: 'CurrentVersion'; version: number } | null>;
+  } | null;
+};
+
 export type GetColonyDecisionByActionIdQueryVariables = Exact<{
   actionId: Scalars['ID'];
 }>;
@@ -10172,6 +10184,15 @@ export const GetColonyContributorDocument = gql`
     getColonyContributor(id: $id) {
       id
       isVerified
+    }
+  }
+`;
+export const GetCurrentColonyVersionDocument = gql`
+  query GetCurrentColonyVersion {
+    getCurrentVersionByKey(key: "colony") {
+      items {
+        version
+      }
     }
   }
 `;
