7ce61e023c69affaf8a2f3ccf146b1e2368b4a94
Bassgeta <kerry@colony.io>
2024-04-18 13:39:16 +0200

    feat: handlers for multi sig extension

--- src/utils/clients/voting.ts
+++ src/utils/clients/voting.ts
@@ -1,4 +1,8 @@
-import { AnyVotingReputationClient, Extension } from '@colony/colony-js';
+import {
+  AnyMultisigPermissionsClient,
+  AnyVotingReputationClient,
+  Extension,
+} from '@colony/colony-js';
 import { getCachedColonyClient } from './colony';
 
 // @TODO: Cache voting clients
@@ -14,6 +18,22 @@ export const getVotingClient = async (
     return await colonyClient.getExtensionClient(Extension.VotingReputation);
   } catch {
     // `getExtensionClient` will throw an error if Voting Rep is not installed, hence the try/catch block
+    return null;
+  }
+};
+
+export const getMultiSigClient = async (
+  colonyAddress: string,
+): Promise<AnyMultisigPermissionsClient | null> => {
+  const colonyClient = await getCachedColonyClient(colonyAddress);
+  if (!colonyClient) {
+    return null;
+  }
+
+  try {
+    return await colonyClient.getExtensionClient(Extension.MultisigPermissions);
+  } catch {
+    // `getExtensionClient` will throw an error if MultiSig is not installed, hence the try/catch block
     return null;
   }
 };
