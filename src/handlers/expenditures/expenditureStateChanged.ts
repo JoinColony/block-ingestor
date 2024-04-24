b7879415f11c37db194c0b287059e2756e2bb972
Jakub Zajac <jakub@colony.io>
2024-04-24 01:26:13 +0100

    Feat: Create edit expenditure action when multicall is present

--- src/handlers/expenditures/expenditureStateChanged.ts
+++ src/handlers/expenditures/expenditureStateChanged.ts
@@ -1,130 +1,80 @@
-import { ContractEvent /*, ContractEventsSignatures */ } from '~types';
+import { mutate } from '~amplifyClient';
+import {
+  UpdateExpenditureDocument,
+  UpdateExpenditureMutation,
+  UpdateExpenditureMutationVariables,
+} from '~graphql';
+import { ContractEvent } from '~types';
 import {
   getCachedColonyClient,
-  // getExpenditureDatabaseId,
-  // mapLogToContractEvent,
-  // output,
-  // toNumber,
-  // verbose,
-  // writeActionFromEvent,
+  getExpenditureDatabaseId,
+  output,
+  toNumber,
+  verbose,
 } from '~utils';
-// import {
-//   ColonyActionType,
-//   ExpenditureSlot,
-//   GetActionByIdDocument,
-//   GetActionByIdQuery,
-//   GetActionByIdQueryVariables,
-//   UpdateColonyActionDocument,
-//   UpdateColonyActionMutation,
-//   UpdateColonyActionMutationVariables,
-//   UpdateExpenditureDocument,
-//   UpdateExpenditureMutation,
-//   UpdateExpenditureMutationVariables,
-// } from '~graphql';
-// import { mutate, query } from '~amplifyClient';
-
-// import {
-//   getExpenditureFromDB,
-//   decodeUpdatedSlot,
-//   decodeUpdatedStatus,
-//   getUpdatedExpenditureSlots,
-// } from './helpers';
-// import provider from '~provider';
-// import { utils } from 'ethers';
-import { createEditExpenditureAction } from './helpers/createEditExpenditureAction';
+import {
+  getExpenditureFromDB,
+  decodeUpdatedSlot,
+  getUpdatedExpenditureSlots,
+  decodeUpdatedStatus,
+  createEditExpenditureAction,
+  CreateEditExpenditureActionResult,
+} from './helpers';
 
 export default async (event: ContractEvent): Promise<void> => {
   const { contractAddress: colonyAddress } = event;
+  const { expenditureId } = event.args;
+  const convertedExpenditureId = toNumber(expenditureId);
+
+  const databaseId = getExpenditureDatabaseId(
+    colonyAddress,
+    convertedExpenditureId,
+  );
+
+  const expenditure = await getExpenditureFromDB(databaseId);
+  if (!expenditure) {
+    output(
+      `Could not find expenditure with ID: ${databaseId} in the db while handling ExpenditureStateChanged event`,
+    );
+    return;
+  }
 
   const colonyClient = await getCachedColonyClient(colonyAddress);
   if (!colonyClient) {
     return;
   }
 
-  await createEditExpenditureAction(event, colonyClient);
+  const result = await createEditExpenditureAction(
+    event,
+    expenditure,
+    colonyClient,
+  );
 
-  // const {
-  //   storageSlot,
-  //   expenditureId,
-  //   value,
-  //   agent: initiatorAddress,
-  // } = event.args;
-  // // The unfortunate naming of the `keys` property means we have to access it like so
-  // const keys = event.args[4];
-  // const convertedExpenditureId = toNumber(expenditureId);
+  if (result === CreateEditExpenditureActionResult.NotEditAction) {
+    const updatedSlot = decodeUpdatedSlot(event, expenditure);
+    const updatedSlots = updatedSlot
+      ? getUpdatedExpenditureSlots(
+          expenditure.slots,
+          updatedSlot.id,
+          updatedSlot,
+        )
+      : undefined;
 
-  // const databaseId = getExpenditureDatabaseId(
-  //   colonyAddress,
-  //   convertedExpenditureId,
-  // );
+    const updatedStatus = decodeUpdatedStatus(event);
 
-  // const expenditure = await getExpenditureFromDB(databaseId);
-  // if (!expenditure) {
-  //   output(
-  //     `Could not find expenditure with ID: ${databaseId} in the db when handling ExpenditureStateChanged event`,
-  //   );
-  //   return;
-  // }
+    verbose(`State of expenditure with ID ${databaseId} changed`);
 
-  // const updatedSlot = decodeUpdatedSlot(expenditure, storageSlot, keys, value);
-
-  // let updatedSlots: ExpenditureSlot[] | undefined;
-  // if (updatedSlot) {
-  //   updatedSlots = getUpdatedExpenditureSlots(
-  //     expenditure.slots,
-  //     updatedSlot.id,
-  //     updatedSlot,
-  //   );
-  // }
-
-  // const updatedStatus = decodeUpdatedStatus(storageSlot, keys, value);
-
-  // verbose(`State of expenditure with ID ${databaseId} updated`);
-
-  // await mutate<UpdateExpenditureMutation, UpdateExpenditureMutationVariables>(
-  //   UpdateExpenditureDocument,
-  //   {
-  //     input: {
-  //       id: databaseId,
-  //       slots: updatedSlots,
-  //       status: updatedStatus,
-  //     },
-  //   },
-  // );
-
-  // const preUpdateSlot = expenditure.slots.find(
-  //   ({ id }) => id === updatedSlot?.id,
-  // );
-
-  // const existingActionQuery = await query<
-  //   GetActionByIdQuery,
-  //   GetActionByIdQueryVariables
-  // >(GetActionByIdDocument, {
-  //   id: transactionHash,
-  // });
-  // if (!existingActionQuery?.data?.getColonyAction) {
-  //   await writeActionFromEvent(event, colonyAddress, {
-  //     type: ColonyActionType.EditExpenditure,
-  //     initiatorAddress,
-  //     expenditureId: databaseId,
-  //     expenditureSlotChanges: {
-  //       oldSlots: preUpdateSlot ? [preUpdateSlot] : [],
-  //       newSlots: updatedSlot ? [updatedSlot] : [],
-  //     },
-  //   });
-  // } else {
-  //   await mutate<
-  //     UpdateColonyActionMutation,
-  //     UpdateColonyActionMutationVariables
-  //   >(UpdateColonyActionDocument, {
-  //     input: {
-  //       id: transactionHash,
-  //       expenditureSlotChanges: {
-  //         oldSlots: preUpdateSlot ? [preUpdateSlot] : [],
-  //         newSlots: updatedSlot ? [updatedSlot] : [],
-  //       },
-  //     },
-  //   });
-  //   console.log('\n\n\n Action existing, should append \n\n\n');
-  // }
+    if (!!updatedSlots || !!updatedStatus) {
+      await mutate<
+        UpdateExpenditureMutation,
+        UpdateExpenditureMutationVariables
+      >(UpdateExpenditureDocument, {
+        input: {
+          id: databaseId,
+          slots: updatedSlots,
+          status: updatedStatus,
+        },
+      });
+    }
+  }
 };
--- src/handlers/expenditures/helpers/index.ts
+++ src/handlers/expenditures/helpers/index.ts
@@ -2,3 +2,4 @@ export * from './getExpenditure';
 export * from './updateExpenditureStake';
 export * from './getUpdatedSlots';
 export * from './decodeSetExpenditureState';
+export * from './createEditExpenditureAction';
