3e279df267eb1047a5e3a11ba2edce875e73aca2
Bassgeta <kerry@colony.io>
2024-04-24 17:17:41 +0200

    feat: implement mint tokens multisig creation and groundwork for more multisig motions

--- src/handlers/multiSig/index.ts
+++ src/handlers/multiSig/index.ts
@@ -1,1 +1,2 @@
 export { default as handleMultiSigGlobalThresholdUpdated } from './globalThresholdUpdated/index';
+export { default as handleMultiSigCreated } from './multiSigCreated/multiSigCreated';
