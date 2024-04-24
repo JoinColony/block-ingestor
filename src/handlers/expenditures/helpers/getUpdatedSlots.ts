5f0ad1b2bfcd9454b608181fb262ac65dec9d20c
Jakub Zajac <jakub@colony.io>
2024-04-24 01:25:32 +0100

    Refactor: merge slot updating utils

--- src/handlers/expenditures/helpers/getUpdatedSlots.ts
+++ src/handlers/expenditures/helpers/getUpdatedSlots.ts
@@ -22,38 +22,3 @@ export const getUpdatedExpenditureSlots = (
 
   return updatedSlots;
 };
-
-/**
- * Function that updates an expenditure slot with a given ID and fields
- * It returns an array of expenditure slots containing the updated slot
- * Requires an array of previously updates slots and will check for matching slots here first
- */
-export const getUpdatedExpenditureSlotsWithHistory = (
-  expenditureSlots: ExpenditureFragment['slots'],
-  slotId: number,
-  fieldsToUpdate: Partial<Omit<ExpenditureSlot, 'id' | '__typename'>>,
-  previouslyUpdatedSlots: ExpenditureFragment['slots'],
-): ExpenditureSlot[] => {
-  let existingSlot;
-
-  const previouslyUpdatedSlot = previouslyUpdatedSlots.find(
-    (slot) => slot.id === slotId,
-  );
-  if (previouslyUpdatedSlot) {
-    existingSlot = previouslyUpdatedSlot;
-  } else {
-    existingSlot = expenditureSlots.find((slot) => slot.id === slotId);
-  }
-  const updatedSlot: ExpenditureSlot = {
-    ...existingSlot,
-    id: slotId,
-    ...fieldsToUpdate,
-  };
-
-  const updatedSlots = [
-    ...previouslyUpdatedSlots.filter((slot) => slot.id !== slotId),
-    updatedSlot,
-  ];
-
-  return updatedSlots;
-};
