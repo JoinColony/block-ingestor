aa64bd9e3fec27b1a880cdf0f1d13c61c95a5e7a
Dave Creaser <david.creaser@outlook.com>
2024-05-07 14:53:36 +0100

    Add streaming payment claims

--- src/handlers/expenditures/streamingPaymentClaimed.ts
+++ src/handlers/expenditures/streamingPaymentClaimed.ts
@@ -1,5 +1,61 @@
+import { mutate } from '~amplifyClient';
+import { ExtensionEventListener } from '~eventListeners';
+import {
+  StreamingPaymentClaim,
+  UpdateStreamingPaymentDocument,
+  UpdateStreamingPaymentMutation,
+  UpdateStreamingPaymentMutationVariables,
+} from '~graphql';
 import { EventHandler } from '~types';
+import {
+  getExpenditureDatabaseId,
+  getStreamingPaymentsClient,
+  output,
+  toNumber,
+} from '~utils';
+import { getStreamingPaymentFromDB } from './helpers';
 
-export const handleStreamingPaymentClaimed: EventHandler = async (event) => {
-  console.log('Streaming payment claimed: ', event);
+export const handleStreamingPaymentClaimed: EventHandler = async (
+  event,
+  listener,
+) => {
+  const { amount, streamingPaymentId, token: tokenAddress } = event.args;
+  const convertedNativeId = toNumber(streamingPaymentId);
+
+  const { colonyAddress } = listener as ExtensionEventListener;
+
+  const streamingPaymentsClient = await getStreamingPaymentsClient(
+    colonyAddress,
+  );
+  if (!streamingPaymentsClient) {
+    return;
+  }
+
+  const databaseId = getExpenditureDatabaseId(colonyAddress, convertedNativeId);
+
+  const streamingPayment = await getStreamingPaymentFromDB(databaseId);
+  if (!streamingPayment) {
+    output(
+      `Could not find streaming payment with ID: ${databaseId} in the db. This is a bug and needs investigating.`,
+    );
+    return;
+  }
+
+  const newClaim: StreamingPaymentClaim = {
+    amount: amount.toString(),
+    tokenAddress,
+  };
+  const claims: StreamingPaymentClaim[] = streamingPayment.claims
+    ? [...streamingPayment.claims, newClaim]
+    : [newClaim];
+
+  await mutate<
+    UpdateStreamingPaymentMutation,
+    UpdateStreamingPaymentMutationVariables
+  >(UpdateStreamingPaymentDocument, {
+    input: {
+      id: databaseId,
+      claims,
+    },
+  });
 };
