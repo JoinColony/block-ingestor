dd08b4e67b26d6f45024057ccfadba43911b01be
Sam Gibbs <34915414+iamsamgibbs@users.noreply.github.com>
2024-05-08 15:44:58 +0100

    Feat: Handle streaming payment startTimeSet

---
+++ src/handlers/expenditures/streamingPaymentStartTimeSet.ts
@@ -0,0 +1,32 @@
+import { mutate } from '~amplifyClient';
+import {
+  UpdateStreamingPaymentDocument,
+  UpdateStreamingPaymentMutation,
+  UpdateStreamingPaymentMutationVariables,
+} from '~graphql';
+import { ContractEvent } from '~types';
+import { getExpenditureDatabaseId, toNumber, verbose } from '~utils';
+
+export default async (event: ContractEvent): Promise<void> => {
+  const { colonyAddress } = event;
+  const { streamingPaymentId, startTime } = event.args;
+  const convertedNativeId = toNumber(streamingPaymentId);
+
+  if (!colonyAddress) {
+    return;
+  }
+
+  const databaseId = getExpenditureDatabaseId(colonyAddress, convertedNativeId);
+
+  verbose(`Start time set for streaming payment with ID ${databaseId}`);
+
+  await mutate<
+    UpdateStreamingPaymentMutation,
+    UpdateStreamingPaymentMutationVariables
+  >(UpdateStreamingPaymentDocument, {
+    input: {
+      id: databaseId,
+      startTime: toNumber(startTime),
+    },
+  });
+};
