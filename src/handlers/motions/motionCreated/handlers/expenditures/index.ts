42b9d972f48112584822b04b58dc8dd49f721fbb
Sam Gibbs <34915414+iamsamgibbs@users.noreply.github.com>
2024-05-17 09:54:26 +0100

    Fix: Release expenditure stage via motion

--- src/handlers/motions/motionCreated/handlers/expenditures/index.ts
+++ src/handlers/motions/motionCreated/handlers/expenditures/index.ts
@@ -1,3 +1,4 @@
 export { default as handleCancelStakedExpenditureMotion } from './cancelStakedExpenditures';
 export { default as handleCancelExpenditureViaArbitrationMotion } from './cancelExpenditureViaArbitration';
 export { default as handleFinalizeExpenditureViaArbitrationMotion } from './finalizeExpenditureViaArbitration';
+export { default as handleReleaseStagedPaymentViaArbitration } from './releaseStagedPaymentViaArbitration';
