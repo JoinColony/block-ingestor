c610fa6da3166473b7b22f59d33e28c81eb9e96c
Sam Gibbs <34915414+iamsamgibbs@users.noreply.github.com>
2024-05-21 12:03:04 +0100

    Fix: Ensure globalThresholdSet does not override domainThresholds

--- src/handlers/multiSig/globalThresholdSet.ts
+++ src/handlers/multiSig/globalThresholdSet.ts
@@ -1,5 +1,8 @@
-import { mutate } from '~amplifyClient';
+import { mutate, query } from '~amplifyClient';
 import {
+  GetColonyExtensionByAddressDocument,
+  GetColonyExtensionByAddressQuery,
+  GetColonyExtensionByAddressQueryVariables,
   UpdateColonyExtensionByAddressDocument,
   UpdateColonyExtensionByAddressMutation,
   UpdateColonyExtensionByAddressMutationVariables,
@@ -10,6 +13,22 @@ export default async (event: ContractEvent): Promise<void> => {
   const { contractAddress: multiSigAddress } = event;
   const { globalThreshold } = event.args;
 
+  const colonyExtensionsResponse = await query<
+    GetColonyExtensionByAddressQuery,
+    GetColonyExtensionByAddressQueryVariables
+  >(GetColonyExtensionByAddressDocument, {
+    extensionAddress: multiSigAddress,
+  });
+
+  const { multiSig } =
+    colonyExtensionsResponse?.data?.getColonyExtension?.params ?? {};
+
+  let colonyDomains;
+
+  if (multiSig) {
+    colonyDomains = [...(multiSig.domainThresholds ?? [])];
+  }
+
   await mutate<
     UpdateColonyExtensionByAddressMutation,
     UpdateColonyExtensionByAddressMutationVariables
@@ -19,6 +38,7 @@ export default async (event: ContractEvent): Promise<void> => {
       params: {
         multiSig: {
           colonyThreshold: globalThreshold.toNumber(),
+          domainThresholds: colonyDomains,
         },
       },
     },
