b36bb10920c4754c1fda75ae6b42c20b32e55d3e
Sam Gibbs <34915414+iamsamgibbs@users.noreply.github.com>
2024-05-23 16:25:30 +0100

    fix: realign after rebase

--- src/graphql/generated.ts
+++ src/graphql/generated.ts
@@ -10152,6 +10152,23 @@ export type GetDomainMetadataQuery = {
   } | null;
 };
 
+export type GetDomainByNativeSkillIdQueryVariables = Exact<{
+  nativeSkillId: Scalars['String'];
+}>;
+
+export type GetDomainByNativeSkillIdQuery = {
+  __typename?: 'Query';
+  getDomainByNativeSkillId?: {
+    __typename?: 'ModelDomainConnection';
+    items: Array<{
+      __typename?: 'Domain';
+      id: string;
+      nativeSkillId: string;
+      nativeId: number;
+    } | null>;
+  } | null;
+};
+
 export type GetDomainsByExtensionAddressQueryVariables = Exact<{
   extensionAddress: Scalars['ID'];
 }>;
@@ -10169,7 +10186,7 @@ export type GetDomainsByExtensionAddressQuery = {
           __typename?: 'ModelDomainConnection';
           items: Array<{
             __typename?: 'Domain';
-            nativeSkillId: number;
+            nativeSkillId: string;
             nativeId: number;
           } | null>;
         } | null;
@@ -11633,6 +11650,17 @@ export const GetDomainMetadataDocument = gql`
         oldDescription
         oldName
         transactionHash
+      }
+    }
+  }
+`;
+export const GetDomainByNativeSkillIdDocument = gql`
+  query GetDomainByNativeSkillId($nativeSkillId: String!) {
+    getDomainByNativeSkillId(nativeSkillId: $nativeSkillId) {
+      items {
+        id
+        nativeSkillId
+        nativeId
       }
     }
   }
