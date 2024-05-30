f4cf4087fb1bd751ab0f19018d36d49f33a3978b
Sam Gibbs <34915414+iamsamgibbs@users.noreply.github.com>
2024-05-30 08:51:24 +0100

    Fix: Remove isRejected from mutation

--- src/graphql/generated.ts
+++ src/graphql/generated.ts
@@ -9762,11 +9762,7 @@ export type UpdateColonyMultiSigMutationVariables = Exact<{
 
 export type UpdateColonyMultiSigMutation = {
   __typename?: 'Mutation';
-  updateColonyMultiSig?: {
-    __typename?: 'ColonyMultiSig';
-    id: string;
-    isRejected: boolean;
-  } | null;
+  updateColonyMultiSig?: { __typename?: 'ColonyMultiSig'; id: string } | null;
 };
 
 export type CreateApprovalVoteMutationVariables = Exact<{
@@ -11460,7 +11456,6 @@ export const UpdateColonyMultiSigDocument = gql`
   mutation UpdateColonyMultiSig($input: UpdateColonyMultiSigInput!) {
     updateColonyMultiSig(input: $input) {
       id
-      isRejected
     }
   }
 `;
