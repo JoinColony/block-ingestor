859f151a6763e6dc746ac586481e51065cd2b459
Bassgeta <kerry@colony.io>
2024-06-06 15:54:34 +0200

    feat: remove all multiSig roles and reject motions upon uninstallation of multi sig extension

--- src/eventListeners/extension/multiSig.ts
+++ src/eventListeners/extension/multiSig.ts
@@ -9,8 +9,24 @@ import {
   handleMultiSigMotionCreated,
   handleMultiSigMotionExecuted,
 } from '~handlers/multiSig';
+import { mutate, query } from '~amplifyClient';
+import {
+  GetActiveColonyMultisigsDocument,
+  GetActiveColonyMultisigsQuery,
+  GetActiveColonyMultisigsQueryVariables,
+  GetAllMultiSigRolesDocument,
+  GetAllMultiSigRolesQuery,
+  GetAllMultiSigRolesQueryVariables,
+  RemoveMultiSigRoleDocument,
+  RemoveMultiSigRoleMutation,
+  RemoveMultiSigRoleMutationVariables,
+  UpdateColonyMultiSigDocument,
+  UpdateColonyMultiSigMutation,
+  UpdateColonyMultiSigMutationVariables,
+} from '~graphql';
 
 import { ContractEventsSignatures } from '~types';
+import { notNull } from '~utils';
 import { addMultiSigParamsToDB } from '~utils/extensions/multiSig';
 import { output } from '~utils/logger';
 
@@ -32,6 +48,56 @@ export const handleMultiSigInstalled = async (
 ): Promise<void> => {
   await addMultiSigParamsToDB(multiSigAddress, colonyAddress);
   setupMultiSigListeners(multiSigAddress, colonyAddress);
+};
+
+export const handleMultiSigUninstalled = async (
+  colonyAddress: string,
+): Promise<void> => {
+  // remove all the roles
+  const multiSigRolesQuery = await query<
+    GetAllMultiSigRolesQuery,
+    GetAllMultiSigRolesQueryVariables
+  >(GetAllMultiSigRolesDocument, {
+    colonyAddress,
+  });
+
+  const roleEntries = multiSigRolesQuery?.data?.listColonyRoles?.items ?? [];
+
+  // @TODO use updateMultiSigInDB when finalize gets merged
+  await Promise.all(
+    roleEntries.filter(notNull).map(async (entry) => {
+      await mutate<
+        RemoveMultiSigRoleMutation,
+        RemoveMultiSigRoleMutationVariables
+      >(RemoveMultiSigRoleDocument, {
+        id: entry.id,
+      });
+    }),
+  );
+
+  const activeMultiSigsQuery = await query<
+    GetActiveColonyMultisigsQuery,
+    GetActiveColonyMultisigsQueryVariables
+  >(GetActiveColonyMultisigsDocument, {
+    colonyAddress,
+  });
+
+  const multiSigs =
+    activeMultiSigsQuery?.data?.listColonyMultiSigs?.items ?? [];
+  await Promise.all(
+    multiSigs.filter(notNull).map(async (entry) => {
+      await mutate<
+        UpdateColonyMultiSigMutation,
+        UpdateColonyMultiSigMutationVariables
+      >(UpdateColonyMultiSigDocument, {
+        input: {
+          id: entry.id,
+          isExecuted: false,
+          isRejected: true,
+        },
+      });
+    }),
+  );
 };
 
 export const setupMultiSigListeners = (
