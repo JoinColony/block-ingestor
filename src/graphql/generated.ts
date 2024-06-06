d3ed92fae5e2f51b21f65206a5dfa0375e0ab8ba
Bassgeta <kerry@colony.io>
2024-06-06 15:54:34 +0200

    feat: remove all multiSig roles and reject motions upon uninstallation of multi sig extension

--- src/graphql/generated.ts
+++ src/graphql/generated.ts
@@ -9915,6 +9915,15 @@ export type RemoveMultiSigVoteMutation = {
   } | null;
 };
 
+export type RemoveMultiSigRoleMutationVariables = Exact<{
+  id: Scalars['ID'];
+}>;
+
+export type RemoveMultiSigRoleMutation = {
+  __typename?: 'Mutation';
+  deleteColonyRole?: { __typename?: 'ColonyRole'; id: string } | null;
+};
+
 export type CreateColonyRoleMutationVariables = Exact<{
   input: CreateColonyRoleInput;
 }>;
@@ -10908,6 +10917,30 @@ export type GetUserMultiSigSignatureQuery = {
   } | null;
 };
 
+export type GetAllMultiSigRolesQueryVariables = Exact<{
+  colonyAddress: Scalars['ID'];
+}>;
+
+export type GetAllMultiSigRolesQuery = {
+  __typename?: 'Query';
+  listColonyRoles?: {
+    __typename?: 'ModelColonyRoleConnection';
+    items: Array<{ __typename?: 'ColonyRole'; id: string } | null>;
+  } | null;
+};
+
+export type GetActiveColonyMultisigsQueryVariables = Exact<{
+  colonyAddress: Scalars['ID'];
+}>;
+
+export type GetActiveColonyMultisigsQuery = {
+  __typename?: 'Query';
+  listColonyMultiSigs?: {
+    __typename?: 'ModelColonyMultiSigConnection';
+    items: Array<{ __typename?: 'ColonyMultiSig'; id: string } | null>;
+  } | null;
+};
+
 export type GetColonyRoleQueryVariables = Exact<{
   id: Scalars['ID'];
 }>;
@@ -11615,6 +11648,13 @@ export const RemoveMultiSigVoteDocument = gql`
     }
   }
 `;
+export const RemoveMultiSigRoleDocument = gql`
+  mutation RemoveMultiSigRole($id: ID!) {
+    deleteColonyRole(input: { id: $id }) {
+      id
+    }
+  }
+`;
 export const CreateColonyRoleDocument = gql`
   mutation CreateColonyRole($input: CreateColonyRoleInput!) {
     createColonyRole(input: $input) {
@@ -12139,6 +12179,37 @@ export const GetUserMultiSigSignatureDocument = gql`
     }
   }
   ${MultiSigUserSignature}
+`;
+export const GetAllMultiSigRolesDocument = gql`
+  query GetAllMultiSigRoles($colonyAddress: ID!) {
+    listColonyRoles(
+      limit: 1000
+      filter: {
+        isMultiSig: { eq: true }
+        colonyAddress: { eq: $colonyAddress }
+      }
+    ) {
+      items {
+        id
+      }
+    }
+  }
+`;
+export const GetActiveColonyMultisigsDocument = gql`
+  query GetActiveColonyMultisigs($colonyAddress: ID!) {
+    listColonyMultiSigs(
+      filter: {
+        colonyAddress: { eq: $colonyAddress }
+        isExecuted: { eq: false }
+        isRejected: { eq: false }
+      }
+      limit: 1000
+    ) {
+      items {
+        id
+      }
+    }
+  }
 `;
 export const GetColonyRoleDocument = gql`
   query GetColonyRole($id: ID!) {
