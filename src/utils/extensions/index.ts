86b4b3ddec104049c373d61bea8447ec18d9a6c2
Jakub Zajac <jakub@colony.io>
2024-06-07 18:40:31 +0100

    Chore: post-rebase fixes

--- src/utils/extensions/index.ts
+++ src/utils/extensions/index.ts
@@ -1,6 +1,5 @@
 export * from './writeExtension';
 export * from './votingRep';
-export * from './stakedExpenditure';
 export * from './installationCount';
 export * from './isAddressExtension';
 export * from './getColonyExtensions';
