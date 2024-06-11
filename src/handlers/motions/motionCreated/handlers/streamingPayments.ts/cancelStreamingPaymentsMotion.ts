c4711ec84fa35a2ee086054e8ceba8126f6faf11
Romeo Ledesma <r_ledesma@outlook.com>
2024-06-11 21:42:23 +0100

    feat: enable handler for streaming payment actions made via motions

---
+++ src/handlers/motions/motionCreated/handlers/streamingPayments.ts/cancelStreamingPaymentsMotion.ts
@@ -0,0 +1,32 @@
+import { BigNumber } from 'ethers';
+import { TransactionDescription } from 'ethers/lib/utils';
+import { ContractEvent, motionNameMapping } from '~types';
+import { createMotionInDB } from '../../helpers';
+import {
+  getDomainDatabaseId,
+  getExpenditureDatabaseId,
+  toNumber,
+} from '~utils';
+
+export default async (
+  colonyAddress: string,
+  event: ContractEvent,
+  { name, args: actionArgs }: TransactionDescription,
+  gasEstimate: BigNumber,
+): Promise<void> => {
+  const { args } = event;
+  const [, , streamingPaymentId] = actionArgs;
+  const [, , domainId] = args;
+
+  await createMotionInDB(colonyAddress, event, {
+    type: motionNameMapping[name],
+    fromDomainId: colonyAddress
+      ? getDomainDatabaseId(colonyAddress, domainId)
+      : undefined,
+    gasEstimate: gasEstimate.toString(),
+    streamingPaymentId: getExpenditureDatabaseId(
+      colonyAddress,
+      toNumber(streamingPaymentId),
+    ),
+  });
+};
