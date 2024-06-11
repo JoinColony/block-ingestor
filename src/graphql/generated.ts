7b451812143addf82502169b481730bc9098100d
Romeo Ledesma <r_ledesma@outlook.com>
2024-06-11 11:21:44 +0100

    feat: handle ClaimWaived event emission

--- src/graphql/generated.ts
+++ src/graphql/generated.ts
@@ -9033,6 +9033,7 @@ export type UpdateStreamingPaymentInput = {
   recipientAddress?: InputMaybe<Scalars['String']>;
   startTime?: InputMaybe<Scalars['AWSTimestamp']>;
   tokenAddress?: InputMaybe<Scalars['String']>;
+  isWaived?: InputMaybe<Scalars['Boolean']>;
 };
 
 export type UpdateStreamingPaymentMetadataInput = {
