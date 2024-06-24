f6c1d2906632cf52b4456ea8c74aeda09783f484
Jakub Zajac <jakub@colony.io>
2024-06-24 16:27:28 +0100

    Feat: Set relevant user stake type on creation

--- src/handlers/motions/motionStaked/helpers.ts
+++ src/handlers/motions/motionStaked/helpers.ts
@@ -14,6 +14,7 @@ import {
   UpdateUserStakeMutation,
   UpdateUserStakeMutationVariables,
   UserMotionStakes,
+  UserStakeType,
 } from '~graphql';
 import { mutate, query } from '~amplifyClient';
 import { getUserStakeDatabaseId } from '~utils/stakes';
@@ -333,6 +334,7 @@ export const updateUserStake = async (
           amount: amount.toString(),
           isClaimed: false,
           createdAt: getBlockChainTimestampISODate(timestamp),
+          type: UserStakeType.Motion,
         },
       },
     );
