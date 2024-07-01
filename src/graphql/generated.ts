50e021b2e2d4caa4e9ee099e7382ed194b2ac465
Bassgeta <kerry@colony.io>
2024-07-01 12:33:01 +0200

    fix: uninstallation listener gql queries

--- src/graphql/generated.ts
+++ src/graphql/generated.ts
@@ -10530,7 +10530,7 @@ export type GetAllMultiSigRolesQueryVariables = Exact<{
 
 export type GetAllMultiSigRolesQuery = {
   __typename?: 'Query';
-  listColonyRoles?: {
+  getRoleByColony?: {
     __typename?: 'ModelColonyRoleConnection';
     items: Array<{ __typename?: 'ColonyRole'; id: string } | null>;
   } | null;
@@ -10542,7 +10542,7 @@ export type GetActiveColonyMultisigsQueryVariables = Exact<{
 
 export type GetActiveColonyMultisigsQuery = {
   __typename?: 'Query';
-  listColonyMultiSigs?: {
+  getMultiSigByColonyAddress?: {
     __typename?: 'ModelColonyMultiSigConnection';
     items: Array<{ __typename?: 'ColonyMultiSig'; id: string } | null>;
   } | null;
@@ -11799,12 +11799,10 @@ export const GetUserMultiSigSignatureDocument = gql`
 `;
 export const GetAllMultiSigRolesDocument = gql`
   query GetAllMultiSigRoles($colonyAddress: ID!) {
-    listColonyRoles(
-      limit: 1000
-      filter: {
-        isMultiSig: { eq: true }
-        colonyAddress: { eq: $colonyAddress }
-      }
+    getRoleByColony(
+      colonyAddress: $colonyAddress
+      limit: 9999
+      filter: { isMultiSig: { eq: true } }
     ) {
       items {
         id
@@ -11814,13 +11812,10 @@ export const GetAllMultiSigRolesDocument = gql`
 `;
 export const GetActiveColonyMultisigsDocument = gql`
   query GetActiveColonyMultisigs($colonyAddress: ID!) {
-    listColonyMultiSigs(
-      filter: {
-        colonyAddress: { eq: $colonyAddress }
-        isExecuted: { eq: false }
-        isRejected: { eq: false }
-      }
-      limit: 1000
+    getMultiSigByColonyAddress(
+      colonyAddress: $colonyAddress
+      filter: { isExecuted: { eq: false }, isRejected: { eq: false } }
+      limit: 9999
     ) {
       items {
         id
