5883616953ff89365d6c35f18183891efb0d14e5
Mihaela-Ioana Mot <32430018+mmioana@users.noreply.github.com>
2024-07-03 16:10:03 +0200

    Fix: Handle manage verified members multi-sig motion
    Handle manage verified members multi-sig motion

---
+++ src/handlers/multiSig/multiSigCreated/handlers/metadataDelta.ts
@@ -0,0 +1,47 @@
+import { TransactionDescription } from 'ethers/lib/utils';
+import { ColonyActionType } from '~graphql';
+import { ContractEvent } from '~types';
+import {
+  isAddVerifiedMembersOperation,
+  isRemoveVerifiedMembersOperation,
+  parseMetadataDeltaOperation,
+  verbose,
+} from '~utils';
+import { createMultiSigInDB } from '../helpers';
+
+export const handleMetadataDeltaMultiSig = async (
+  colonyAddress: string,
+  event: ContractEvent,
+  desc: TransactionDescription,
+): Promise<void> => {
+  try {
+    const operationString = desc.args[0];
+
+    if (!operationString) {
+      verbose('Unable to get operation for ColonyMetadataDelta motion event');
+      return;
+    }
+
+    const operation = parseMetadataDeltaOperation(operationString);
+
+    if (operation === null) {
+      return;
+    }
+
+    if (isAddVerifiedMembersOperation(operation)) {
+      await createMultiSigInDB(colonyAddress, event, {
+        type: ColonyActionType.AddVerifiedMembersMotion,
+        members: operation.payload,
+      });
+    }
+
+    if (isRemoveVerifiedMembersOperation(operation)) {
+      await createMultiSigInDB(colonyAddress, event, {
+        type: ColonyActionType.RemoveVerifiedMembersMotion,
+        members: operation.payload,
+      });
+    }
+  } catch (error) {
+    verbose('Error while handling metadata delta motion created', error);
+  }
+};
