9aa1e0097be0f9d41104ae861c821442d924f897
Dave Creaser <david.creaser@outlook.com>
2024-07-04 17:22:49 +0100

    Add move funds multisig handler

---
+++ src/handlers/multiSig/multiSigCreated/handlers/moveFunds.ts
@@ -0,0 +1,68 @@
+import { TransactionDescription } from 'ethers/lib/utils';
+import { ContractEvent, multiSigNameMapping } from '~types';
+import {
+  getCachedColonyClient,
+  getDomainDatabaseId,
+  isDomainFromFundingPotSupported,
+  toNumber,
+} from '~utils';
+import { createMultiSigInDB } from '../helpers';
+import { BigNumber } from 'ethers';
+
+export const handleMoveFundsMultiSig = async (
+  colonyAddress: string,
+  event: ContractEvent,
+  parsedAction: TransactionDescription,
+): Promise<void> => {
+  const { blockNumber } = event;
+  if (!colonyAddress) {
+    return;
+  }
+
+  const { name, args: actionArgs } = parsedAction;
+
+  const colonyClient = await getCachedColonyClient(colonyAddress);
+
+  if (!colonyClient) {
+    return;
+  }
+
+  const colonyVersion = await colonyClient.version({ blockTag: blockNumber });
+
+  // There are two moveFundsBetweenPots actions, one pre colony version 7 and one post.
+  let fromPot: BigNumber,
+    toPot: BigNumber,
+    amount: BigNumber,
+    tokenAddress: string;
+
+  const isOldVersion = colonyVersion.lte(6);
+  if (isOldVersion) {
+    [, , , fromPot, toPot, amount, tokenAddress] = actionArgs;
+  } else {
+    [, , , , , fromPot, toPot, amount, tokenAddress] = actionArgs;
+  }
+
+  let fromDomainId: BigNumber | undefined;
+  let toDomainId: BigNumber | undefined;
+
+  if (isDomainFromFundingPotSupported(colonyClient)) {
+    fromDomainId = await colonyClient.getDomainFromFundingPot(fromPot, {
+      blockTag: blockNumber,
+    });
+    toDomainId = await colonyClient.getDomainFromFundingPot(toPot, {
+      blockTag: blockNumber,
+    });
+  }
+
+  await createMultiSigInDB(colonyAddress, event, {
+    type: multiSigNameMapping[name],
+    tokenAddress,
+    amount: amount.toString(),
+    fromDomainId: fromDomainId
+      ? getDomainDatabaseId(colonyAddress, toNumber(fromDomainId))
+      : undefined,
+    toDomainId: toDomainId
+      ? getDomainDatabaseId(colonyAddress, toNumber(toDomainId))
+      : undefined,
+  });
+};
