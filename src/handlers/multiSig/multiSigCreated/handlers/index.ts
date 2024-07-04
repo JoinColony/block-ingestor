fa6db535ecfdecab897ca70edb254d06b20d0408
Mihaela-Ioana Mot <32430018+mmioana@users.noreply.github.com>
2024-07-04 16:26:22 +0200

    Feat: Edit colony using multi-sig
    Edit colony using multi-sig

--- src/handlers/multiSig/multiSigCreated/handlers/index.ts
+++ src/handlers/multiSig/multiSigCreated/handlers/index.ts
@@ -1,3 +1,4 @@
+export { handleEditColonyMotion } from './editColony';
 export { handleMintTokensMultiSig } from './mintTokens';
 export { handleMoveFundsMultiSig } from './moveFunds';
 export { handleUnlockTokenMultiSig } from './unlockToken';
