88ec373568cbd86e1ea390b84675aea4e0090c10
Bassgeta <kerry@colony.io>
2024-07-09 16:27:11 +0200

    feat: handle multiple user multisig signatures

--- src/graphql/generated.ts
+++ src/graphql/generated.ts
@@ -10573,6 +10573,7 @@ export type GetUserMultiSigSignatureQueryVariables = Exact<{
   multiSigId: Scalars['ID'];
   userAddress: Scalars['ID'];
   vote: MultiSigVote;
+  role: Scalars['Int'];
 }>;
 
 export type GetUserMultiSigSignatureQuery = {
@@ -11863,9 +11864,14 @@ export const GetUserMultiSigSignatureDocument = gql`
     $multiSigId: ID!
     $userAddress: ID!
     $vote: MultiSigVote!
+    $role: Int!
   ) {
     getMultiSigUserSignatureByMultiSigId(
-      filter: { userAddress: { eq: $userAddress }, vote: { eq: $vote } }
+      filter: {
+        userAddress: { eq: $userAddress }
+        vote: { eq: $vote }
+        role: { eq: $role }
+      }
       multiSigId: $multiSigId
     ) {
       items {
