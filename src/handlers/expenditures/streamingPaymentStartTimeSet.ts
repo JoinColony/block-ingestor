2f4642824850ba567fc5d782ce700c0f31d8f74c
Sam Gibbs <34915414+iamsamgibbs@users.noreply.github.com>
2024-07-18 15:54:33 +0100

    Feat: Refactor editStreamingPayment handlers and ensure colony action is created

--- src/handlers/expenditures/streamingPaymentStartTimeSet.ts
+++ src/handlers/expenditures/streamingPaymentStartTimeSet.ts
@@ -1,13 +1,7 @@
-import { mutate } from '~amplifyClient';
-import {
-  UpdateStreamingPaymentDocument,
-  UpdateStreamingPaymentMutation,
-  UpdateStreamingPaymentMutationVariables,
-} from '~graphql';
 import { EventHandler } from '~types';
-import { getExpenditureDatabaseId, output, toNumber, verbose } from '~utils';
-import { getStreamingPaymentFromDB } from './helpers';
+import { getStreamingPaymentsClient } from '~utils';
 import { ExtensionEventListener } from '~eventListeners';
+import { createEditStreamingPaymentAction } from './helpers/createEditStreamingPaymentAction';
 
 export const handleStreamingPaymentStartTimeSet: EventHandler = async (
   event,
@@ -15,31 +9,20 @@ export const handleStreamingPaymentStartTimeSet: EventHandler = async (
 ) => {
   const { colonyAddress } = listener as ExtensionEventListener;
 
-  const { streamingPaymentId, startTime } = event.args;
-  const convertedNativeId = toNumber(streamingPaymentId);
-
   if (!colonyAddress) {
     return;
   }
 
-  const databaseId = getExpenditureDatabaseId(colonyAddress, convertedNativeId);
-  const streamingPayment = await getStreamingPaymentFromDB(databaseId);
-  if (!streamingPayment) {
-    output(
-      `Could not find streaming payment with ID: ${databaseId} in the db. This is a bug and needs investigating.`,
-    );
+  const streamingPaymentsClient = await getStreamingPaymentsClient(
+    colonyAddress,
+  );
+  if (!streamingPaymentsClient) {
     return;
   }
 
-  verbose(`Start time set for streaming payment with ID ${databaseId}`);
-
-  await mutate<
-    UpdateStreamingPaymentMutation,
-    UpdateStreamingPaymentMutationVariables
-  >(UpdateStreamingPaymentDocument, {
-    input: {
-      id: databaseId,
-      startTime: startTime.toString(),
-    },
+  await createEditStreamingPaymentAction({
+    event,
+    streamingPaymentsClient,
+    colonyAddress,
   });
 };
