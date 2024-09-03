dc6c50f8d5ab8f9a23f088f15b45123a895da8ed
Dave Creaser <david.creaser@outlook.com>
2024-09-03 10:14:49 +0100

    Send notifications on simple payment action

--- src/utils/graphql.ts
+++ src/utils/graphql.ts
@@ -5,6 +5,37 @@ import { query } from '~amplifyClient';
 
 dotenv.config();
 
+interface GetDataResponse<TItem> {
+  items: Array<TItem | null> | undefined;
+  nextToken?: string | null;
+}
+
+export type GetDataFn<T, K> = (
+  params: K,
+  nextToken?: string | null,
+) => Promise<GetDataResponse<T> | null | undefined>;
+
+export const getAllPagesOfData = async <T, K>(
+  getDataFunc: GetDataFn<T, K>,
+  params: K,
+): Promise<Array<T | null>> => {
+  const items = [];
+  let nextToken = null;
+
+  do {
+    const data: GetDataResponse<T> | null | undefined = await getDataFunc(
+      { ...params },
+      nextToken,
+    );
+    nextToken = data?.nextToken;
+    if (data?.items) {
+      items.push(...data.items);
+    }
+  } while (nextToken);
+
+  return items;
+};
+
 export const delay = async (timeout: number): Promise<void> => {
   return await new Promise((resolve) => {
     setTimeout(resolve, timeout);
