1faeb3231dc64e476cc826dccb07f06b4e6f67b3
Dave Creaser <david.creaser@outlook.com>
2024-10-18 13:22:53 +0100

    Only fire one notification on multisig approval

--- src/handlers/multiSig/approvalChanged/approvalChanged.ts
+++ src/handlers/multiSig/approvalChanged/approvalChanged.ts
@@ -1,3 +1,4 @@
+import { BigNumber } from 'ethers';
 import { ExtensionEventListener } from '~eventListeners';
 import { MultiSigVote, NotificationType } from '~graphql';
 import { getChainId } from '~provider';
@@ -66,7 +67,45 @@ export const handleMultiSigApprovalChanged: EventHandler = async (
       multiSigFromDB?.action?.type,
     );
 
-    if (notificationCategory && !motion.overallApprovalTimestamp.isZero()) {
+    let hasFullApproval = true;
+    let roleIndex = 0;
+
+    // Map over each permission.
+    while (
+      BigNumber.from(motion.requiredPermissions).gte(
+        BigNumber.from(1).shl(roleIndex),
+      )
+    ) {
+      // If the permission is required by this multi sig motion...
+      // (this logic is taken from the network, it's very cool I know)
+      if (
+        BigNumber.from(motion.requiredPermissions)
+          .and(BigNumber.from(1).shl(roleIndex))
+          .gt(BigNumber.from(0))
+      ) {
+        // Get the threshold for this role from the multi sig motion.
+        const thresholdForRole = await multiSigClient.getMotionVoteThreshold(
+          motionId,
+          role,
+        );
+
+        // Get the number of signatures for this role that have voted approve.
+        const approvalsForRole =
+          multiSigFromDB?.signatures?.items.filter(
+            (signature) =>
+              signature?.role === roleIndex &&
+              signature.vote === MultiSigVote.Approve,
+          ) ?? [];
+
+        // If the number of approval signatures for this role is less than the threshold, then the multi sig motion has not reached full approval.
+        if (thresholdForRole.gt(BigNumber.from(approvalsForRole.length))) {
+          hasFullApproval = false;
+        }
+      }
+      roleIndex += 1;
+    }
+
+    if (notificationCategory && hasFullApproval) {
       sendMultisigActionNotifications({
         colonyAddress,
         creator: userAddress,
