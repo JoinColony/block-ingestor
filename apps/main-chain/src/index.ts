ed068d4f0a243b00b6b75c0f4713db0cec2e8b72
Mihaela-Ioana Mot <32430018+mmioana@users.noreply.github.com>
2024-12-02 07:57:21 +0100

    Refactor: Remove logs
    Refactor: Remove logs

--- apps/main-chain/src/index.ts
+++ apps/main-chain/src/index.ts
@@ -14,10 +14,6 @@ import { setupNotificationsClient } from '~utils/notifications';
 
 utils.Logger.setLogLevel(utils.Logger.levels.ERROR);
 
-//@TODO remove these after testing
-console.log(`Magicbell dev key var`, process.env.MAGICBELL_DEV_KEY)
-console.log(`RPC endpoint var`, process.env.CHAIN_RPC_ENDPOINT)
-
 const start = async (): Promise<void> => {
   amplifyClientSetup();
 
