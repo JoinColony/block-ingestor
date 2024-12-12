e31dd85629c42b7144a4fd19e9f8edd962bf7bb2
Anastasiia <anastasiia@colony.io>
2024-12-12 20:43:46 +0100

    feat: handle arbitrary transaction motion

--- src/handlers/motions/motionCreated/handlers/makeArbitraryTransactions.ts
+++ src/handlers/motions/motionCreated/handlers/makeArbitraryTransactions.ts
@@ -1,7 +1,11 @@
 import { TransactionDescription } from 'ethers/lib/utils';
 import { Id } from '@colony/colony-js';
 import { ContractEvent, motionNameMapping } from '~types';
-import { getDomainDatabaseId } from '~utils';
+import {
+  argsByTypeToString,
+  decodeArbitraryFunction,
+  getDomainDatabaseId,
+} from '~utils';
 
 import { createMotionInDB } from '../helpers';
 
@@ -13,9 +17,39 @@ export const handleMakeArbitraryTransactionsMotion = async (
   const { name, args: actionArgs } = parsedAction;
   const [recipients] = actionArgs;
 
+  const { _targets: contractAddresses, _actions: encodedFunctions } =
+    parsedAction.args;
+
+  const currentArbitraryTransactions = await Promise.all(
+    contractAddresses.map(async (contractAddress: string, index: number) => {
+      const currentEncodedFunction = encodedFunctions[index];
+      const decodedFunction = await decodeArbitraryFunction(
+        currentEncodedFunction,
+      );
+      const functionInputs = decodedFunction?.functionFragment.inputs;
+      const functionArgs = decodedFunction?.args;
+
+      const mappedArgs = functionInputs?.map((item, index) => {
+        return {
+          name: item.name,
+          type: item.type,
+          value: argsByTypeToString(functionArgs?.[index], item.type),
+        };
+      });
+      return {
+        contractAddress,
+        method: decodedFunction?.name,
+        methodSignature: decodedFunction?.signature,
+        args: mappedArgs,
+        encodedFunction: currentEncodedFunction,
+      };
+    }),
+  );
+
   await createMotionInDB(colonyAddress, event, {
     type: motionNameMapping[name],
     recipientAddress: recipients[0],
     fromDomainId: getDomainDatabaseId(colonyAddress, Id.RootDomain),
+    arbitraryTransactions: currentArbitraryTransactions,
   });
 };
