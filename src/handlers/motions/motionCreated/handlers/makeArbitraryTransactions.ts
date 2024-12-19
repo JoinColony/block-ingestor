de081f29d5e9f842c218841f4cdf06df5177ceaa
Anastasiia <anastasiia@colony.io>
2024-12-19 16:17:18 +0100

    feat: refactor accoring to Jakub's refactoring changes

--- src/handlers/motions/motionCreated/handlers/makeArbitraryTransactions.ts
+++ src/handlers/motions/motionCreated/handlers/makeArbitraryTransactions.ts
@@ -1,11 +1,7 @@
 import { TransactionDescription } from 'ethers/lib/utils';
 import { Id } from '@colony/colony-js';
 import { ContractEvent, motionNameMapping } from '~types';
-import {
-  argsByTypeToString,
-  decodeArbitraryFunction,
-  getDomainDatabaseId,
-} from '~utils';
+import { getDomainDatabaseId } from '~utils';
 
 import { createMotionInDB } from '../helpers';
 
@@ -22,24 +18,9 @@ export const handleMakeArbitraryTransactionsMotion = async (
   const currentArbitraryTransactions = await Promise.all(
     contractAddresses.map(async (contractAddress: string, index: number) => {
       const currentEncodedFunction = encodedFunctions[index];
-      const decodedFunction = await decodeArbitraryFunction(
-        currentEncodedFunction,
-      );
-      const functionInputs = decodedFunction?.functionFragment.inputs;
-      const functionArgs = decodedFunction?.args;
 
-      const mappedArgs = functionInputs?.map((item, index) => {
-        return {
-          name: item.name,
-          type: item.type,
-          value: argsByTypeToString(functionArgs?.[index], item.type),
-        };
-      });
       return {
         contractAddress,
-        method: decodedFunction?.name,
-        methodSignature: decodedFunction?.signature,
-        args: mappedArgs,
         encodedFunction: currentEncodedFunction,
       };
     }),
