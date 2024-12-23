59880ff3a841c3eb4ed08e05022be7ff162bc1ec
Mihaela-Ioana Mot <32430018+mmioana@users.noreply.github.com>
2024-12-23 22:42:01 +0100

    Feat: Handle disable proxy colony metadataDelta event
    Feat: Handle disable proxy colony metadataDelta event

--- apps/main-chain/src/handlers/metadataDelta/metadataDelta.ts
+++ apps/main-chain/src/handlers/metadataDelta/metadataDelta.ts
@@ -1,6 +1,7 @@
 import { ContractEvent } from '@joincolony/blocks';
 import {
   isAddVerifiedMembersOperation,
+  isDisableProxyColonyOperation,
   isManageTokensOperation,
   isRemoveVerifiedMembersOperation,
   parseMetadataDeltaOperation,
@@ -9,6 +10,7 @@ import { handleAddVerifiedMembers } from './handlers/addVerifiedMembers';
 import { handleRemoveVerifiedMembers } from './handlers/removeVerifiedMembers';
 import { handleManageTokens } from './handlers/manageTokens';
 import { verbose } from '@joincolony/utils';
+import { handleDisableProxyColony } from './handlers/disableProxyColony';
 
 export default async (event: ContractEvent): Promise<void> => {
   const operationString = event.args.metadata;
@@ -31,6 +33,10 @@ export default async (event: ContractEvent): Promise<void> => {
   if (isManageTokensOperation(operation)) {
     await handleManageTokens(event, operation);
     return;
+  }
+
+  if (isDisableProxyColonyOperation(operation)) {
+    await handleDisableProxyColony(event, operation);
   }
 
   verbose('Unknown operation type: ', operation);
