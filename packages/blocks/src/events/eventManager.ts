f33134b2c166f87b815e43e65a3bf6b9242d5e99
Bassgeta <kerry@colony.io>
2025-01-06 14:26:23 +0100

    feat: get colonyAddress when requesting a proxy colony and the initiator address

--- packages/blocks/src/events/eventManager.ts
+++ packages/blocks/src/events/eventManager.ts
@@ -17,7 +17,7 @@ import { Extension, getExtensionHash } from '@colony/colony-js';
 
 // @TODO @chmanie is gonna make this better, for now let's just hardcode the proxy colony events
 export const ProxyColonyEvents = new utils.Interface([
-  'event ProxyColonyRequested(uint256 destinationChainId, bytes32 salt)',
+  'event ProxyColonyRequested(address colony,uint256 destinationChainId, bytes32 salt)',
   'event ProxyColonyDeployed(address proxyColony)',
   // @TODO decouple these into MultiChainBridgeEvents
   'event WormholeMessageReceived(uint16 emitterChainId, bytes32 emitterAddress, uint64 sequence)',
