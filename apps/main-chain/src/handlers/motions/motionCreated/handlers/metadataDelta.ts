a6214e6b223c2e90698bfc8b893a68c607d93997
Romeo Ledesma <r_ledesma@outlook.com>
2025-01-10 02:50:30 +0800

    feat: handle enabling and disabling of a proxy colony via a motion

--- apps/main-chain/src/handlers/motions/motionCreated/handlers/metadataDelta.ts
+++ apps/main-chain/src/handlers/motions/motionCreated/handlers/metadataDelta.ts
@@ -3,6 +3,8 @@ import { ColonyActionType } from '@joincolony/graphql';
 import { ContractEvent } from '@joincolony/blocks';
 import {
   isAddVerifiedMembersOperation,
+  isDisableProxyColonyOperation,
+  isEnableProxyColonyOperation,
   isManageTokensOperation,
   isRemoveVerifiedMembersOperation,
   parseMetadataDeltaOperation,
@@ -49,6 +51,27 @@ export const handleMetadataDeltaMotion = async (
         colonyAddress,
         event,
         operation,
+      });
+    }
+
+    if (
+      isDisableProxyColonyOperation(operation) ||
+      isEnableProxyColonyOperation(operation)
+    ) {
+      const targetChainId = JSON.parse(desc.args[0]).payload[0];
+
+      if (!targetChainId) {
+        return;
+      }
+
+      await createMotionInDB(colonyAddress, event, {
+        type: isDisableProxyColonyOperation(operation)
+          ? ColonyActionType.RemoveProxyColonyMotion
+          : ColonyActionType.AddProxyColonyMotion,
+        multiChainInfo: {
+          completed: false,
+          targetChainId: Number(targetChainId),
+        },
       });
     }
   } catch (error) {
